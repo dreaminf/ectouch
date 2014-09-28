20d4d129e57087615c7d9b485022015f29c28cc5
vagrant <wanganlin@ecmoban.com>
2014-09-28 19:45:25 +0800

    删除多余的控制器文件

--- respond.php
+++ respond.php
@@ -15,12 +15,6 @@
 
 /* 访问控制 */
 define('IN_ECTOUCH', true);
-/* 设置系统编码格式 */
-header("Content-Type:text/html;charset=utf-8");
-/* 设置系统编码格式 */
-header("Pragma: no-cache");
-/* 修复后退没有提交数据的问题 */
-header("Cache-control: private");
 define('DEFAULT_APP', 'default');
 define('DEFAULT_CONTROLLER', 'Respond');
 /* 加载核心文件 */
