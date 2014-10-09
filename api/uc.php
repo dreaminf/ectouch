ced18820b89fa7de9d316fcf6ead7faa35baaee8
vagrant <wanganlin@ecmoban.com>
2014-10-09 18:03:45 +0800

    合并uc接口文件

--- api/uc.php
+++ api/uc.php
@@ -35,8 +35,35 @@ define('API_RETURN_FAILED', '-1');
 define('API_RETURN_FORBIDDEN', '-2');
 define('EC_CHARSET', 'utf-8');
 
-define('IN_ECTOUCH', TRUE);
-require './init.php';
+define('ROOT_PATH', realpath(dirname(__FILE__) . '/../') . '/');
+define('BASE_PATH', ROOT_PATH . 'include/');
+/* 加载默认配置 */
+require (BASE_PATH . 'EcConfig.class.php');
+/* 加载默认配置 */
+require (BASE_PATH . 'Common.php');
+/* 加载常规配置 */
+C(load_file(BASE_PATH . 'Convention.php'));
+/* 设置时区 */
+date_default_timezone_set(EcConfig::get('TIMEZONE'));
+require (ROOT_PATH . 'include/base/function.php');
+/* 初始化数据库类 */
+require (ROOT_PATH . 'include/EcModel.class.php');
+$db = new EcModel(C('DB'));
+
+/* 初始化session */
+require (ROOT_PATH . 'include/library/EcsApiSession.class.php');
+$sess_name = defined("SESS_NAME") ? SESS_NAME : 'ECS_ID';
+$sess = new EcsApiSession($db, $db->pre .'sessions', $db->pre . 'sessions_data', $sess_name);
+
+/* 载入系统参数 */
+$_CFG = load_config();
+C('CFG', $_CFG);
+
+/* 初始化用户插件 */
+$user = init_users();
+
+header('Content-type: text/html; charset=' . EC_CHARSET);
+
 // 数据验证
 if (! defined('IN_UC')) {
     error_reporting(0);
