19b2e7ab1d90036cc7c275eb2ded632d70e34a6a
vagrant <wanganlin@ecmoban.com>
2014-10-10 11:55:53 +0800

    修改授权模板

--- include/apps/admin/view/index_license.php
+++ include/apps/admin/view/index_license.php
@@ -4,10 +4,10 @@
     <h3 class="panel-title">{$ur_here}</h3>
   </div>
   <div class="panel-body">
-    <form action="{url('empower')}" method="post" enctype="multipart/form-data" class="form-horizontal" role="form">
+    <form action="{url('license')}" method="post" enctype="multipart/form-data" class="form-horizontal" role="form">
       <table id="general-table" class="table table-hover ectouch-table">
         <tr>
-          <td width="200">授权卡号:</td>
+          <td width="200">授权序列号:</td>
           <td><div class="col-md-4">
               <input type='text' name='license' maxlength="20" class="form-control input-sm" />
             </div></td>
