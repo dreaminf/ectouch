6e784f96e909251f43f80da023541b5f7080c752
vagrant <wanganlin@ecmoban.com>
2014-10-16 14:19:12 +0800

    更新默认图片

--- include/base/function.php
+++ include/base/function.php
@@ -1747,10 +1747,8 @@ function get_image_path($goods_id, $image = '', $thumb = false, $call = 'goods',
  * @return type
  */
 function get_banner_path($img) {
-    $url = empty($img) ? C('no_picture') : $img;
-    $base_url = substr(__ROOT__, -1) == '/' ? __ROOT__ : __ROOT__ . '/';
-    $url = strtolower(substr($url, 0, '4')) == 'http' ? $url : $base_url . $url;
-    return $url;
+    $img = empty($img) ? C('no_picture') : $img;
+    return $img;
 }
 
 /**
