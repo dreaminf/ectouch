f5acf0ecad3ed8e88d279146f73cf3cd0fd15639
vagrant <wanganlin@ecmoban.com>
2014-10-19 12:06:29 +0800

    增加URL重定向函数

--- include/Common.php
+++ include/Common.php
@@ -577,7 +577,6 @@ function U($url='',$vars='',$suffix=true,$domain=false) {
                 $var[$varController] = parse_name($var[$varController]);
             }
             $module =   '';
-            
             if(!empty($path)) {
                 $var[$varModule] = implode($depr,$path);
             }else{
@@ -668,6 +667,35 @@ function is_ssl() {
         return true;
     }
     return false;
+}
+
+/**
+ * URL重定向
+ * @param string $url 重定向的URL地址
+ * @param integer $time 重定向的等待时间（秒）
+ * @param string $msg 重定向前的提示信息
+ * @return void
+ */
+function redirect($url, $time=0, $msg='') {
+    //多行URL地址支持
+    $url = str_replace(array("\n", "\r"), '', $url);
+    if (empty($msg))
+        $msg    = "系统将在{$time}秒之后自动跳转到{$url}！";
+    if (!headers_sent()) {
+        // redirect
+        if (0 === $time) {
+            header('Location: ' . $url);
+        } else {
+            header("refresh:{$time};url={$url}");
+            echo($msg);
+        }
+        exit();
+    } else {
+        $str    = "<meta http-equiv='Refresh' content='{$time};URL={$url}'>";
+        if ($time != 0)
+            $str .= $msg;
+        exit($str);
+    }
 }
 
 /**
