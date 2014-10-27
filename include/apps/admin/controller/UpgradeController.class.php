94d41423acb1860e4dd6cf452d117ffa9ad5c0b0
vagrant <wanganlin@ecmoban.com>
2014-10-27 18:10:14 +0800

    优化在线更新，增加文件校验功能

--- include/apps/admin/controller/UpgradeController.class.php
+++ include/apps/admin/controller/UpgradeController.class.php
@@ -19,12 +19,8 @@ defined('IN_ECTOUCH') or die('Deny Access');
 class UpgradeController extends AdminController
 {
 
-    private $_filearr = array(
-        'api',
-        'phpcms',
-        'statics',
-        ''
-    );
+    private $md5_arr = array();
+    private $_filearr = array('admin', 'api', 'include', 'plugins', '');
     // md5验证地址
     private $_upgrademd5 = 'http://ectouch.cn/upgrademd5/';
     // 补丁地址
@@ -36,16 +32,167 @@ class UpgradeController extends AdminController
     public function __construct()
     {
         parent::__construct();
+        $this->patch_charset = str_replace('-', '', EC_CHARSET);
+        $this->upgrade_path_base = $this->_patchurl . $this->patch_charset . '/';
+        defined('CACHE_PATH') or define('CACHE_PATH', ROOT_PATH.'data/cache/');
     }
 
     /**
-     * 升级补丁
+     * 升级列表
      */
     public function index()
     {
-        $patch_charset = str_replace('-', '', EC_CHARSET);
-        $upgrade_path_base = $this->_patchurl . $patch_charset . '/';
-        $pathlist_str = Http::doGet($upgrade_path_base);
+        $pathlist = $this->pathlist();
+        $this->assign('pathlist', $pathlist);
+        $this->display();
+    }
+
+    /**
+     * 在线升级
+     */
+    public function init()
+    {
+        // 开始升级
+        $do = I('get.do');
+        $cover = I('cover', 0);
+        if (empty($do)) {
+            $this->message(L('upgradeing'), url('init', array('do'=>1, 'cover'=> $cover)));
+        }
+        // 获取补丁列表
+        $pathlist = $this->pathlist();
+        if (empty($pathlist)) {
+            $this->message(L('upgrade_success'), url('checkfile'));
+        }
+        // 创建缓存文件夹
+        if (! file_exists(CACHE_PATH . 'upgrade')) {
+            @mkdir(CACHE_PATH . 'upgrade');
+        }
+
+        foreach ($pathlist as $k => $v) {
+            $release = str_replace('patch_R', '', basename($v, ".zip"));
+            // 远程压缩包地址
+            $upgradezip_url = $this->upgrade_path_base . $v;
+            // 保存到本地地址
+            $upgradezip_path = CACHE_PATH . 'upgrade' . DIRECTORY_SEPARATOR . $v;
+            // 解压路径
+            $upgradezip_source_path = CACHE_PATH . 'upgrade' . DIRECTORY_SEPARATOR . basename($v, ".zip");
+            // 下载压缩包
+            @file_put_contents($upgradezip_path, Http::doGet($upgradezip_url));
+            // 解压缩
+            $zip = new Zip();
+            if ($zip->decompress($upgradezip_path, $upgradezip_source_path) == 0) {
+                die("Error : unpack the failure.");
+            }
+            // 拷贝utf8/upload文件夹到根目录
+            $copy_from = $upgradezip_source_path . DIRECTORY_SEPARATOR . $this->patch_charset . DIRECTORY_SEPARATOR . 'upload' . DIRECTORY_SEPARATOR;
+            $copy_to = ROOT_PATH;
+            
+            $this->copyfailnum = 0;
+            $this->copydir($copy_from, $copy_to, $cover);
+            // 检查文件操作权限，是否复制成功
+            if ($this->copyfailnum > 0) {
+                // 如果失败，终止操作
+                $this->message(L('please_check_filepri'), url('index'));
+            }
+
+            // 执行sql
+            // sql目录地址
+            $sql_path = CACHE_PATH . 'upgrade' . DIRECTORY_SEPARATOR . basename($v, ".zip") . DIRECTORY_SEPARATOR . $this->patch_charset . DIRECTORY_SEPARATOR . 'upgrade' . DIRECTORY_SEPARATOR;
+            $file_list = glob($sql_path . '*');
+            if (! empty($file_list)) {
+                foreach ($file_list as $fk => $fv) {
+                    $file_path = strtolower($fv);
+                    if (in_array(substr($file_path, - 3, 3), array('php', 'sql')) && substr($file_path, - 10, 10) != 'config.php') {
+                        if (substr($file_path, - 3, 3) == 'sql') {
+                            // 更新sql文件
+                            $sqlData = Install::mysql($file_path, 'ecs_', C('DB_PREFIX'));
+                            // 执行sql文件
+                            $model = new EcModel();
+                            if (is_array($sqlData)){
+                                foreach ($sqlData as $sql) {
+                                    @$model->db->query($sql);
+                                }
+                            }
+                        } elseif (strtolower(substr($file_list[$fk], - 3, 3)) == 'php' && file_exists($file_path)) {
+                            include $file_path;
+                        }
+                    }
+                }
+            }
+            
+            // 读取版本号写入version.php文件
+            // 配置文件地址
+            $configpath = CACHE_PATH . 'upgrade' . DIRECTORY_SEPARATOR . basename($v, ".zip") . DIRECTORY_SEPARATOR . $this->patch_charset . DIRECTORY_SEPARATOR . 'upgrade' . DIRECTORY_SEPARATOR . 'version.php';
+            if (file_exists($configpath)) {
+                $config = include $configpath;
+                // 版本文件地址
+                $content = "<?php\ndefine('APPNAME', '".$config['APPNAME']."');\ndefine('VERSION', '".$config['VERSION']."');\ndefine('RELEASE', '".$release."');\ndefine('ECTOUCH_AUTH_KEY', '".ECTOUCH_AUTH_KEY."');";
+                @file_put_contents(ROOT_PATH . 'data/version.php', $content);
+            }
+            
+            // 删除文件
+            @unlink($upgradezip_path);
+            // 删除文件夹
+            del_dir($upgradezip_source_path);
+            
+            // 提示语
+            $tmp_k = $k + 1;
+            if (! empty($pathlist[$tmp_k])) {
+                $next_update = '<br />' . L('upgradeing') . basename($pathlist[$tmp_k], ".zip");
+            } else {
+                $next_update;
+            }
+            // 是否升级成功
+            $this->message(basename($v, ".zip") . L('upgrade_success') . $next_update, url('init', array('do'=>1, 'cover'=> $cover)));
+        }
+        
+    }
+    
+    // 检查文件md5值
+    public function checkfile()
+    {
+        $do = I('get.do', 0);
+        if (! empty($do)) {
+            $this->ec_readdir('.');
+            // 读取接口
+            $ectouch_md5 = Http::doGet($this->_upgrademd5 . RELEASE . '_' . $this->patch_charset . ".php");
+            $ectouch_md5_arr = json_decode($ectouch_md5, 1);
+            // 计算数组差集
+            $diff = array_diff($ectouch_md5_arr, $this->md5_arr);
+            // 丢失文件列表
+            $lostfile = array();
+            foreach ($ectouch_md5_arr as $k => $v) {
+                if (! in_array($k, array_keys($this->md5_arr))) {
+                    $lostfile[] = $k;
+                    unset($diff[$k]);
+                }
+            }
+            // 未知文件列表
+            $unknowfile = array_diff(array_keys($this->md5_arr), array_keys($ectouch_md5_arr));
+            // 赋值
+            $this->assign('diff', $diff);
+            $this->assign('lostfile', $lostfile);
+            $this->assign('unknowfile', $unknowfile);
+            $this->display();
+        } else {
+            $this->message(L('begin_checkfile'), url('checkfile', array('do'=> 1)));
+        }
+    }
+    
+    /**
+     * 生成核心文件md5
+     */
+    public function buildhash(){
+        $this->ec_readdir('.');
+        file_put_contents(CACHE_PATH . RELEASE . '_' . $this->patch_charset.'.php', json_encode($this->md5_arr));
+        $this->message(L('build_success'), url('index'));
+    }
+    
+    /**
+     * 获取补丁列表
+     */
+    private function pathlist(){
+        $pathlist_str = Http::doGet($this->upgrade_path_base);
         $pathlist = $allpathlist = array();
         $key = - 1;
         // 获取压缩包列表
@@ -64,204 +211,28 @@ class UpgradeController extends AdminController
                 $pathlist[$k] = $v;
             }
         }
-        $this->assign('pathlist', $pathlist);
-        $this->display();
+        return $pathlist;
     }
 
-    public function updating()
-    {
-        // 开始升级
-        if (! empty($_GET['s'])) {
-            if (empty($_GET['do'])) {
-                showmessage(L('upgradeing'), '?m=upgrade&c=index&a=init&s=1&do=1&cover=' . $_GET['cover']);
-            }
-            // 检查服务器是否支持zip
-            
-            if (empty($pathlist)) {
-                showmessage(L('upgrade_success'), '?m=upgrade&c=index&a=checkfile');
-            }
-            
-            // 创建缓存文件夹
-            if (! file_exists(CACHE_PATH . 'caches_upgrade')) {
-                @mkdir(CACHE_PATH . 'caches_upgrade');
-            }
-            
-            // 根据版本下载zip升级包，解压覆盖
-            pc_base::load_app_class('pclzip', 'upgrade', 0);
-            
-            foreach ($pathlist as $k => $v) {
-                // 远程压缩包地址
-                $upgradezip_url = $upgrade_path_base . $v;
-                // 保存到本地地址
-                $upgradezip_path = CACHE_PATH . 'caches_upgrade' . DIRECTORY_SEPARATOR . $v;
-                // 解压路径
-                $upgradezip_source_path = CACHE_PATH . 'caches_upgrade' . DIRECTORY_SEPARATOR . basename($v, ".zip");
-                
-                // 下载压缩包
-                @file_put_contents($upgradezip_path, @file_get_contents($upgradezip_url));
-                // 解压缩
-                $archive = new PclZip($upgradezip_path);
-                
-                if ($archive->extract(PCLZIP_OPT_PATH, $upgradezip_source_path, PCLZIP_OPT_REPLACE_NEWER) == 0) {
-                    die("Error : " . $archive->errorInfo(true));
-                }
-                
-                // 拷贝gbk/upload文件夹到根目录
-                $copy_from = $upgradezip_source_path . DIRECTORY_SEPARATOR . $patch_charset . DIRECTORY_SEPARATOR . 'upload' . DIRECTORY_SEPARATOR;
-                $copy_to = PHPCMS_PATH;
-                
-                $this->copyfailnum = 0;
-                $this->copydir($copy_from, $copy_to, $_GET['cover']);
-                
-                // 检查文件操作权限，是否复制成功
-                if ($this->copyfailnum > 0) {
-                    // 如果失败，恢复当前版本
-                    @file_put_contents(CACHE_PATH . 'configs' . DIRECTORY_SEPARATOR . 'version.php', '<?php return ' . var_export($current_version, true) . ';?>');
-                    
-                    showmessage(L('please_check_filepri'));
-                }
-                
-                // 执行sql
-                // sql目录地址
-                $sql_path = CACHE_PATH . 'caches_upgrade' . DIRECTORY_SEPARATOR . basename($v, ".zip") . DIRECTORY_SEPARATOR . $patch_charset . DIRECTORY_SEPARATOR . 'upgrade' . DIRECTORY_SEPARATOR . 'ext' . DIRECTORY_SEPARATOR;
-                $file_list = glob($sql_path . '*');
-                if (! empty($file_list)) {
-                    foreach ($file_list as $fk => $fv) {
-                        if (in_array(strtolower(substr($fv, - 3, 3)), array(
-                            'php',
-                            'sql'
-                        ))) {
-                            if (strtolower(substr($file_list[$fk], - 3, 3)) == 'sql' && $data = file_get_contents($file_list[$fk])) {
-                                $model_name = substr(basename($fv), 0, - 4);
-                                if (! $db = pc_base::load_model($model_name . '_model')) {
-                                    showmessage($model_name . L('lost'), '?m=upgrade&c=index&a=init&s=1');
-                                }
-                                $mysql_server_version = $db->version();
-                                $dbcharset = pc_base::load_config('database', 'default');
-                                $dbcharset = $dbcharset['charset'];
-                                
-                                $sqls = explode(';', $data);
-                                foreach ($sqls as $sql) {
-                                    if (empty($sql))
-                                        continue;
-                                    if (mysql_get_server_info > '4.1' && $dbcharset) {
-                                        $sql = preg_replace("/TYPE=(InnoDB|MyISAM|MEMORY)( DEFAULT CHARSET=[^; ]+)?/", "TYPE=\\1 DEFAULT CHARSET=" . $dbcharset, $sql);
-                                    }
-                                    $db->query($sql);
-                                }
-                            } elseif (strtolower(substr($file_list[$fk], - 3, 3)) == 'php' && file_exists($file_list[$fk])) {
-                                include $file_list[$fk];
-                                
-                                // 同步菜单语言包
-                                if (strtolower(basename($file_list[$fk])) == 'system_menu.lang.php' && file_exists($file_list[$fk])) {
-                                    include $file_list[$fk];
-                                    $new_lan = $LANG;
-                                    unset($LANG);
-                                    $lang = pc_base::load_config('system', 'lang');
-                                    $menu_lan_file = PC_PATH . 'languages' . DIRECTORY_SEPARATOR . $lang . DIRECTORY_SEPARATOR . 'system_menu.lang.php';
-                                    include $menu_lan_file;
-                                    $original_lan = $LANG;
-                                    unset($LANG);
-                                    $diff_lan = array_diff($new_lan, $original_lan);
-                                    
-                                    $content = file_get_contents($menu_lan_file);
-                                    $content = substr($content, 0, - 2);
-                                    $data = '';
-                                    foreach ($diff_lan as $lk => $l) {
-                                        $data .= "\$LANG['" . $lk . "'] = '" . $l . "';\n\r";
-                                    }
-                                    $data = $content . $data . "?>";
-                                    file_put_contents($menu_lan_file, $data);
-                                }
-                            }
-                        }
-                    }
-                }
-                
-                // 读取版本号写入version.php文件
-                // 配置文件地址
-                $configpath = CACHE_PATH . 'caches_upgrade' . DIRECTORY_SEPARATOR . basename($v, ".zip") . DIRECTORY_SEPARATOR . $patch_charset . DIRECTORY_SEPARATOR . 'upgrade' . DIRECTORY_SEPARATOR . 'config.php';
-                if (file_exists($configpath)) {
-                    $config_arr = include $configpath;
-                    $version_arr = array(
-                        'pc_version' => $config_arr['to_version'],
-                        'pc_release' => $config_arr['to_release']
-                    );
-                    // 版本文件地址
-                    $version_filepath = CACHE_PATH . 'configs' . DIRECTORY_SEPARATOR . 'version.php';
-                    @file_put_contents($version_filepath, '<?php return ' . var_export($version_arr, true) . ';?>');
-                }
-                
-                // 删除文件
-                @unlink($upgradezip_path);
-                // 删除文件夹
-                $this->deletedir($upgradezip_source_path);
-                
-                // 提示语
-                $tmp_k = $k + 1;
-                if (! empty($pathlist[$tmp_k])) {
-                    $next_update = '<br />' . L('upgradeing') . basename($pathlist[$tmp_k], ".zip");
-                } else {
-                    $next_update;
-                }
-                // 文件校验是否升级成功
-                showmessage(basename($v, ".zip") . L('upgrade_success') . $next_update, '?m=upgrade&c=index&a=init&s=1&do=1&cover=' . $_GET['cover']);
-            }
-        }
-    }
-    
-    // 检查文件md5值
-    public function checkfile()
-    {
-        if (! empty($_GET['do'])) {
-            $this->md5_arr = array();
-            $this->_pc_readdir(".");
-            
-            // 读取phpcms接口
-            $current_version = pc_base::load_config('version');
-            $phpcms_md5 = @file_get_contents($this->_upgrademd5 . $current_version['pc_release'] . '_' . CHARSET . ".php");
-            $phpcms_md5_arr = json_decode($phpcms_md5, 1);
-            // 计算数组差集
-            $diff = array_diff($phpcms_md5_arr, $this->md5_arr);
-            
-            // 丢失文件列表
-            $lostfile = array();
-            foreach ($phpcms_md5_arr as $k => $v) {
-                if (! in_array($k, array_keys($this->md5_arr))) {
-                    $lostfile[] = $k;
-                    unset($diff[$k]);
-                }
-            }
-            
-            // 未知文件列表
-            $unknowfile = array_diff(array_keys($this->md5_arr), array_keys($phpcms_md5_arr));
-            
-            include $this->admin_tpl('check_file');
-        } else {
-            showmessage(L('begin_checkfile'), '?m=upgrade&c=index&a=checkfile&do=1&menuid=' . $_GET['menuid']);
-        }
-    }
-
-    private function _pc_readdir($path = '')
+    private function ec_readdir($path = '')
     {
         $dir_arr = explode('/', dirname($path));
         if (is_dir($path)) {
             $handler = opendir($path);
             while (($filename = @readdir($handler)) !== false) {
                 if (substr($filename, 0, 1) != ".") {
-                    $this->_pc_readdir($path . '/' . $filename);
+                    $this->ec_readdir($path . '/' . $filename);
                 }
             }
             closedir($handler);
         } else {
-            
             if (dirname($path) == '.' || (isset($dir_arr[1]) && in_array($dir_arr[1], $this->_filearr))) {
                 $this->md5_arr[base64_encode($path)] = md5_file($path);
             }
         }
     }
 
-    public function copydir($dirfrom, $dirto, $cover = '')
+    private function copydir($dirfrom, $dirto, $cover = '')
     {
         // 如果遇到同名文件无法复制，则直接退出
         if (is_file($dirto)) {
@@ -290,7 +261,7 @@ class UpgradeController extends AdminController
                             echo L('copy') . $filefrom . L('to') . $fileto . L('failed') . "<br />";
                         }
                     } else {
-                        if (fileext($fileto) == 'html' && file_exists($fileto)) {} else {
+                        if ((get_extension($fileto) == 'dwt' || get_extension($fileto) == 'lbi') && file_exists($fileto)) {} else {
                             if (! copy($filefrom, $fileto)) {
                                 $this->copyfailnum ++;
                                 echo L('copy') . $filefrom . L('to') . $fileto . L('failed') . "<br />";
@@ -301,24 +272,5 @@ class UpgradeController extends AdminController
             }
         }
     }
-
-    function deletedir($dirname)
-    {
-        $result = false;
-        if (! is_dir($dirname)) {
-            echo " $dirname is not a dir!";
-            exit(0);
-        }
-        $handle = opendir($dirname); // 打开目录
-        while (($file = readdir($handle)) !== false) {
-            if ($file != '.' && $file != '..') { // 排除"."和"."
-                $dir = $dirname . DIRECTORY_SEPARATOR . $file;
-                // $dir是目录时递归调用deletedir,是文件则直接删除
-                is_dir($dir) ? $this->deletedir($dir) : unlink($dir);
-            }
-        }
-        closedir($handle);
-        $result = rmdir($dirname) ? true : false;
-        return $result;
-    }
+    
 }
--- include/apps/admin/view/upgrade_index.php
+++ include/apps/admin/view/upgrade_index.php
@@ -7,14 +7,9 @@
   </div>
 </div>
 
-<p class="bg-danger" style="padding: 10px;line-height: 24px;">{$lang['upgrade_notice']}</p>
+<p class="bg-success" style="padding: 10px;line-height: 24px;">{$lang['upgrade_notice']}</p>
 
-<form name="myform" action="" method="get" id="myform">
-  <input type="hidden" name="s" value="1" />
-  <input type="hidden" name="cover" value="<?php echo $_GET['cover']?>" />
-  <input name="m" value="upgrade" type="hidden" />
-  <input name="c" value="index" type="hidden" />
-  <input name="a" value="init" type="hidden" />
+<form action="{url('init')}" method="post">
   <div class="panel panel-default">
     <div class="panel-heading">
       <h3 class="panel-title">{$lang['upgrade']}</h3>
@@ -22,39 +17,37 @@
     <div class="panel-body" style="padding:0;">
       <table class="table table-hover ectouch-table">
         <tr>
-          <td width="40%">{$lang['currentversion']}
+          <td width="70%">{$lang['currentversion']}
             <?php if(empty($pathlist)){ echo $lang['lastversion']; }?></td>
-          <td width="60%">{$lang['updatetime']}</td>
+          <td width="30%" class="text-right">{$lang['updatetime']}</td>
         </tr>
         <tr>
           <td>{VERSION}</td>
-          <td>{RELEASE}</td>
+          <td class="text-right">{RELEASE}</td>
         </tr>
       </table>
     </div>
   </div>
   <?php if(!empty($pathlist)) {?>
-  <div class="bk15"></div>
-  <table width="100%" cellspacing="0">
-    <thead>
-      <tr>
-        <th align="left" width="300"><?php echo L('updatelist')?></th>
-        <th align="left"><?php echo L('updatetime')?></th>
-      </tr>
-    </thead>
-    <tbody>
-      <?php foreach($pathlist as $v) { ?>
-      <tr>
-        <td><?php echo $v;?></td>
-        <td><?php echo substr($v, 15, 8);?></td>
-      </tr>
-      <?php }?>
-    </tbody>
+  <table class="table table-hover ectouch-table" style="border:1px #ddd solid; margin-bottom:10px;">
+    <tr>
+      <th width="70%">{$lang['updatelist']}</th>
+      <th width="30%" class="text-right">{$lang['updatetime']}</th>
+    </tr>
+    <?php foreach($pathlist as $v) { ?>
+    <tr>
+      <td><?php echo $v;?></td>
+      <td class="text-right"><?php echo substr($v, 7, 8);?></td>
+    </tr>
+    <?php }?>
   </table>
-  <div class="bk15"></div>
-  <label for="cover"><font color="red"><?php echo L('covertemplate')?></font></label>
-  <input name="cover" id="cover" type="checkbox" value=1>
-  <input name="dosubmit" type="submit" id="dosubmit" value="<?php echo L('begin_upgrade')?>" class="button">
+  <p>
+  <input name="cover" id="cover" type="checkbox" value="1" />
+  <label for="cover"><font color="red">{$lang['covertemplate']}</font></label>
+  </p>
+  <p>
+  <input name="dosubmit" type="submit" id="dosubmit" value="{$lang['begin_upgrade']}" class="btn btn-warning" />
+  </p>
   <?php }?>
 </form>
 {include file="pagefooter"}
