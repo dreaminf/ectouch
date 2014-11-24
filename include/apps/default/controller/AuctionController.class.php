93015290e282d5db1015183bcec6cf2c73c04b0e
Zhulin <zhulin@ecmoban.com>
2014-11-24 15:31:48 +0800

    新增拍卖活动

---
+++ include/apps/default/controller/AuctionController.class.php
@@ -0,0 +1,89 @@
+<?php
+
+/**
+ * ECTouch Open Source Project
+ * ============================================================================
+ * Copyright (c) 2012-2014 http://ectouch.cn All rights reserved.
+ * ----------------------------------------------------------------------------
+ * 文件名称：ActivityControoller.class.php
+ * ----------------------------------------------------------------------------
+ * 功能描述：拍卖活动控制器
+ * ----------------------------------------------------------------------------
+ * Licensed ( http://www.ectouch.cn/docs/license.txt )
+ * ----------------------------------------------------------------------------
+ */
+/* 访问控制 */
+defined('IN_ECTOUCH') or die('Deny Access');
+
+class AuctionController extends CommonController {
+
+    private $size = 10;
+    private $page = 1;
+    private $sort = 'last_update';
+    private $order = 'ASC';
+
+    /**
+     * 构造函数
+     */
+    public function __construct() {
+        parent::__construct();
+    }
+
+    /**
+     * 拍卖活动列表
+     */
+    public function index() {
+        $this->parameter();
+        $this->assign('page', $this->page);
+        $this->assign('size', $this->size);
+        $this->assign('sort', $this->sort);
+        $this->assign('order', $this->order);
+        /* 取得拍卖活动总数 */
+        $count = model('Auction')->auction_count();
+        /* 如果没有缓存，生成缓存 */
+        if ($count > 0) {
+            /* 取得当前页的拍卖活动 */
+            $auction_list = model('Auction')->auction_list($this->size, $this->page);
+            $this->assign('auction_list', $auction_list);
+            /* 设置分页链接 */
+            $this->pageLimit(url('index', array('sort' => $this->sort, 'order' => $this->order)), $this->size);
+            $this->assign('pager', $this->pageShow($count));
+        }
+        $this->display('auction_list.dwt');
+    }
+
+    /**
+     * 处理参数便于搜索商品信息
+     */
+    private function parameter() {
+        // 如果分类ID为0，则返回总分类页
+        $page_size = C('page_size');
+        $this->size = intval($page_size) > 0 ? intval($page_size) : 10;
+        $this->page = I('request.page') ? intval(I('request.page')) : 1;
+        /* 排序、显示方式以及类型 */
+        $default_display_type = C('show_order_type') == '0' ? 'list' : (C('show_order_type') == '1' ? 'grid' : 'album');
+        $default_sort_order_method = C('sort_order_method') == '0' ? 'DESC' : 'ASC';
+        $default_sort_order_type = C('sort_order_type') == '0' ? 'goods_id' : (C('sort_order_type') == '1' ? 'shop_price' : 'last_update');
+
+        $this->assign('show_asynclist', C('show_asynclist'));
+        $this->sort = (isset($_REQUEST['sort']) && in_array(trim(strtolower($_REQUEST['sort'])), array(
+                    'goods_id',
+                    'shop_price',
+                    'last_update',
+                    'sales_volume',
+                    'click_count'
+                ))) ? trim($_REQUEST['sort']) : $default_sort_order_type; // 增加按人气、按销量排序 by wang
+        $this->order = (isset($_REQUEST['order']) && in_array(trim(strtoupper($_REQUEST['order'])), array(
+                    'ASC',
+                    'DESC'
+                ))) ? trim($_REQUEST['order']) : $default_sort_order_method;
+        $display = (isset($_REQUEST['display']) && in_array(trim(strtolower($_REQUEST['display'])), array(
+                    'list',
+                    'grid',
+                    'album'
+                ))) ? trim($_REQUEST['display']) : (isset($_COOKIE['ECS']['display']) ? $_COOKIE['ECS']['display'] : $default_display_type);
+        $this->assign('display', $display);
+        setcookie('ECS[display]', $display, gmtime() + 86400 * 7);
+    }
+
+}
---
+++ include/apps/default/model/AuctionModel.class.php
@@ -0,0 +1,81 @@
+<?php
+
+/**
+ * ECTouch Open Source Project
+ * ============================================================================
+ * Copyright (c) 2012-2014 http://ectouch.cn All rights reserved.
+ * ----------------------------------------------------------------------------
+ * 文件名称：ActivityModel.class.php
+ * ----------------------------------------------------------------------------
+ * 功能描述：ECTOUCH 拍卖活动模型
+ * ----------------------------------------------------------------------------
+ * Licensed ( http://www.ectouch.cn/docs/license.txt )
+ * ----------------------------------------------------------------------------
+ */
+/* 访问控制 */
+defined('IN_ECTOUCH') or die('Deny Access');
+
+class AuctionModel extends BaseModel {
+
+    /**
+     * 取得拍卖活动数量
+     * @return  int
+     */
+    function auction_count() {
+        $now = gmtime();
+        $sql = "SELECT COUNT(*) as count " .
+                "FROM " . $this->pre .
+                "goods_activity WHERE act_type = '" . GAT_AUCTION . "' " .
+                "AND start_time <= '$now' AND end_time >= '$now' AND is_finished < 2";
+        $res = $this->row($sql);
+        return $res['count'];
+    }
+
+    /**
+     * 取得某页的拍卖活动
+     * @param   int     $size   每页记录数
+     * @param   int     $page   当前页
+     * @return  array
+     */
+    function auction_list($size, $page) {
+        $auction_list = array();
+        $auction_list['finished'] = $auction_list['finished'] = array();
+
+        $now = gmtime();
+        $start = ($page - 1) * $size;
+        $sql = "SELECT a.*,t.act_banner ,t.sales_count ,t.click_num ,  IFNULL(g.goods_thumb, '') AS goods_thumb " .
+                "FROM " . $this->pre . "goods_activity AS a " .
+                "LEFT JOIN " . $this->pre . "goods AS g ON a.goods_id = g.goods_id " .
+                "LEFT JOIN " . $this->pre . "touch_goods_activity AS t ON a.act_id = t.act_id " .
+                "LEFT JOIN " . $this->pre . "touch_goods as tg ON g.goods_id = tg.goods_id " .
+                "WHERE a.act_type = '" . GAT_AUCTION . "' " .
+                "AND a.start_time <= '$now' AND a.end_time >= '$now' AND a.is_finished < 2 ORDER BY a.act_id DESC LIMIT $start ,$size ";
+        $res = $this->query($sql);
+
+        foreach ($res as $row) {
+            $ext_info = unserialize($row['ext_info']);
+            $auction = array_merge($row, $ext_info);
+            $auction['status_no'] = auction_status($auction);
+
+            $auction['start_time'] = local_date($GLOBALS['_CFG']['time_format'], $auction['start_time']);
+            $auction['end_time'] = local_date($GLOBALS['_CFG']['time_format'], $auction['end_time']);
+            $auction['formated_start_price'] = price_format($auction['start_price']);
+            $auction['formated_end_price'] = price_format($auction['end_price']);
+            $auction['formated_deposit'] = price_format($auction['deposit']);
+            $auction['goods_thumb'] = get_image_path($row['goods_id'], $row['goods_thumb'], true);
+            $auction['act_banner'] = $row['act_banner'] ? $row['act_banner'] : $auction['goods_thumb'];
+            $auction['url'] = build_uri('auction', array('auid' => $auction['act_id']));
+
+            if ($auction['status_no'] < 2) {
+                $auction_list['under_way'][] = $auction;
+            } else {
+                $auction_list['finished'][] = $auction;
+            }
+        }
+
+
+        $auction_list = @array_merge($auction_list['under_way'], $auction_list['finished']);
+        return $auction_list;
+    }
+
+}
