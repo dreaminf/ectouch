63a31eeb3ec49c3d484f19773d50b8901733deec
carson <wanganlin@ecmoban.com>
2015-02-26 17:48:37 +0800

    移除对支付宝wap支付的限制

--- include/apps/default/model/OrderModel.class.php
+++ include/apps/default/model/OrderModel.class.php
@@ -54,10 +54,9 @@ class OrderModel extends BaseModel {
      * @return  array   配送方式数组
      */
     function available_payment_list($support_cod, $cod_fee = 0, $is_online = false) {
-        $condition = (is_wechat_browser()) ? " AND pay_code <> 'alipay_wap'":'';
         $sql = 'SELECT pay_id, pay_code, pay_name, pay_fee, pay_desc, pay_config, is_cod' .
                 ' FROM ' . $this->pre .
-                'touch_payment WHERE enabled = 1 '.$condition;
+                'touch_payment WHERE enabled = 1 ';
         if (!$support_cod) {
             $sql .= 'AND is_cod = 0 '; // 如果不支持货到付款
         }
