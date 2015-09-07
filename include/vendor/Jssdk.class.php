1cd0a2d2a35f6516b83a2bd5c5eb56bdb9931a77
carson <docxcn@gmail.com>
2015-09-07 10:02:02 +0800

    优化curl请求

--- include/vendor/Jssdk.class.php
+++ include/vendor/Jssdk.class.php
@@ -87,17 +87,7 @@ class Jssdk {
   }
 
   private function httpGet($url) {
-    $curl = curl_init();
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($curl, CURLOPT_TIMEOUT, 500);
-    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-    curl_setopt($curl, CURLOPT_URL, $url);
-
-    $res = curl_exec($curl);
-    curl_close($curl);
-
-    return $res;
+    return Http::doGet($url);
   }
 }
 
