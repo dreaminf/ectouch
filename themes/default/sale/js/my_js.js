f890f7d9562c8c5675db2d7f98bbea606230ccfa
wangshuai <wangshuai@ecmoban.com>
2015-09-17 10:03:10 +0800

    修复我的商品页面点击商品分销报错问题

--- themes/default/sale/js/my_js.js
+++ themes/default/sale/js/my_js.js
@@ -3,23 +3,44 @@ $(function(){
 		$(this).next(".index_nav").slideToggle("slow").siblings(".div3:visible").slideUp("slow");
 	});
 });
+
+/**
+ * 我的商品
+ * @param id
+ */
 function select_cate(id){
     if($("#cate_"+id).hasClass("sp_box1")){
-        $("#input_"+id).attr("checked",'true');
+        $("#input_"+id).prop("checked",true);
         $("#cate_"+id).removeClass("sp_box1").addClass("sp_box2");
-        $("#cate_"+id+" span").removeClass("font-color-7a").addClass("font-color-fff");
     }else{
-
-        $("#input_"+id).removeAttr("checked");
+        $("#input_"+id).prop("checked",false);
         $("#cate_"+id).removeClass("sp_box2").addClass("sp_box1");
-        $("#cate_"+id+" span").removeClass("font-color-fff").addClass("font-color-7a7a7a");
-        if($("#checkAll").is(":checked")==true){
-            $("#checkAll").attr("checked",false);
-        }
     }
 
 }
+function set_my_goods(){
+    var id_arr = 0;
+    $('input[name="cate[]"]:checked').each(function(){
+        id_arr++;
+    });
+    if(id_arr > 0){
+        $("#my_goods").submit();
+    }else{
+        alert('请选择分销分类');
+    }
+}
+function check_all(){
+    if($("#checkAll").is(":checked")==true){
+        $(".elecheck").prop("checked",true);
+        $(".check").removeClass("sp_box1").addClass("sp_box2");
 
+    }else{
+        $(".elecheck").prop("checked",false);
+        $(".check").removeClass("sp_box2").addClass("sp_box1");
+
+    }
+
+}
 
 // 关闭指定class
 function close_class(id){
