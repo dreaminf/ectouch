bd94f3316b93ec0dc3934fd0ed3681c45e6a1e8e
carson <docxcn@gmail.com>
2015-09-18 15:48:31 +0800

    优化应用程序目录

--- config/config.php
+++ config/config.php
@@ -218,7 +218,7 @@ $config['log_threshold'] = 0;
 | application/logs/ directory. Use a full server path with trailing slash.
 |
 */
-$config['log_path'] = '';
+$config['log_path'] = 'caches/logs/';
 
 /*
 |--------------------------------------------------------------------------
@@ -277,7 +277,7 @@ $config['error_views_path'] = '';
 | application/cache/ directory.  Use a full server path with trailing slash.
 |
 */
-$config['cache_path'] = '';
+$config['cache_path'] = 'caches/';
 
 /*
 |--------------------------------------------------------------------------
