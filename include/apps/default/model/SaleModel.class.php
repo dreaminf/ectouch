abc6593788dfdc647531be3e0878b3853eb44bd2
wangshuai <wangshuai@ecmoban.com>
2015-09-28 15:58:48 +0800

    调试分销佣金

--- include/apps/default/model/SaleModel.class.php
+++ include/apps/default/model/SaleModel.class.php
@@ -139,10 +139,7 @@ class SaleModel extends BaseModel {
     function saleMoney($uid=0) {
         $uid = $uid > 0 ? $uid : $_SESSION['user_id'];
         $money = M()->getRow("select sum(user_money) as money from {pre}drp_log where user_id = ".$uid ." and user_money > 0");
-        dump($money);
         $money = $money['money'];
-        dump("select sum(user_money) as money from {pre}drp_log where user_id = ".$uid ." and user_money > 0");
-        dump($money);
         return $money ? $money : 0;
 
     }
