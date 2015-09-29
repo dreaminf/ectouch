ad5b1f68a313d08e08092fd15532ca11bda140cd
wangshuai <wangshuai@ecmoban.com>
2015-09-29 14:10:06 +0800

    调试关注公众号会员

--- include/apps/default/model/SaleModel.class.php
+++ include/apps/default/model/SaleModel.class.php
@@ -472,8 +472,9 @@ class SaleModel extends BaseModel {
             $res['count'] = count($user_list);
             $res['list'] = $user_list;
         }elseif($key == 'gz'){
-            $sql = "SELECT user_id FROM {pre}users  as u join {pre}wechat_user as w on u.user_id=w.ect_uid where u.user_id > 0 and w.subscribe = 1 and u.parent_id = " . $_SESSION['user_id'] . " GROUP BY user_id";
+           echo  $sql = "SELECT user_id FROM {pre}users  as u join {pre}wechat_user as w on u.user_id=w.ect_uid where u.user_id > 0 and w.subscribe = 1 and u.parent_id = " . $_SESSION['user_id'] . " GROUP BY user_id";
             $user_list =  M()->query($sql);
+            dump($user_list);
             $res['count'] = count($user_list);
             $res['list'] = $user_list;
         }elseif($key == 'fk'){
