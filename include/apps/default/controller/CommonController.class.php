3cb9cbf9ce83d545a38638f2a1fb58b3c51ec4e5
wangshuai <wangshuai@ecmoban.com>
2015-09-29 17:29:46 +0800

    更新首页店铺信息

--- include/apps/default/controller/CommonController.class.php
+++ include/apps/default/controller/CommonController.class.php
@@ -196,6 +196,16 @@ class CommonController extends BaseController
                 $drp_info['cat_id'] = substr($drp_info['cat_id'], 0, -1);
                 $_SESSION['drp_shop'] = $drp_info;
 
+            }else{
+                $parent_id = $this->model->table('users')->field('parent_id')->where("user_id=".$_SESSION['user_id'])->getOne();
+                if($parent_id){
+                    $drp_info = model('Sale')->get_drp($parent_id);
+                    if($drp_info['open'] == 1) {
+                        $drp_info['cat_id'] = substr($drp_info['cat_id'], 0, -1);
+                        $_SESSION['drp_shop'] = $drp_info;
+                    }
+                }
+
             }
         }
     }
