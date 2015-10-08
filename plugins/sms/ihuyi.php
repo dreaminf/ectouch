740a2dc5535105c715e9205e154ea8ef940d2ab0
carson <docxcn@gmail.com>
2015-10-08 15:48:31 +0800

    增加短信接口模块

---
+++ plugins/sms/ihuyi.php
@@ -0,0 +1,26 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class ihuyi {
+
+    /**
+     * 短信发送接口
+     */
+    public function send(){
+
+    }
+
+    /**
+     * 状态报告接口
+     */
+    public function query(){
+
+    }
+
+    /**
+     * 余额及已发送量查询接口
+     */
+    public function overage(){
+
+    }
+}
