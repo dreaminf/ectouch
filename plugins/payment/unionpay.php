3ff36f3c1c79509563d8f3ae69b3a645b1de5040
carson <docxcn@gmail.com>
2015-10-08 15:48:10 +0800

    更新支付插件，移除配置模块

--- plugins/payment/unionpay.php
+++ plugins/payment/unionpay.php
@@ -13,40 +13,6 @@
  * ----------------------------------------------------------------------------
  */
 
-$payment_lang = ROOT_PATH . 'language/' .C('lang'). '/payment/unionpay.php';
-if (file_exists($payment_lang)) {
-    global $_LANG;
-    include_once($payment_lang);
-}
-
-/* 模块的基本信息 */
-if (isset($set_modules) && $set_modules == TRUE) {
-    $i = isset($modules) ? count($modules) : 0;
-    /* 代码 */
-    $modules[$i]['code'] = basename(__FILE__, '.php');
-    /* 描述对应的语言项 */
-    $modules[$i]['desc'] = 'unionpay_desc';
-    /* 是否货到付款 */
-    $modules[$i]['is_cod'] = '0';
-    /* 是否支持在线支付 */
-    $modules[$i]['is_online'] = '1';
-    /* 作者 */
-    $modules[$i]['author'] = 'ECTouch TEAM';
-    /* 网址 */
-    $modules[$i]['website'] = 'http://www.ecshop.com';
-    /* 版本号 */
-    $modules[$i]['version'] = '1.0.0';
-    /* 配置信息 */
-    $modules[$i]['config'] = array(
-        array(
-            'name' => 'mer_id',
-            'type' => 'text',
-            'value' => ''
-        ),
-    );
-    // 签名证书密码
-    return;
-}
 defined('SDK_SIGN_CERT_PATH') or define('SDK_SIGN_CERT_PATH', DATA_PATH . '/cer/' . 'PM_700000000000001_acp.pfx');
 defined('SDK_SIGN_CERT_PWD') or define('SDK_SIGN_CERT_PWD', '000000');
 defined('SDK_VERIFY_CERT_DIR') or define('SDK_VERIFY_CERT_DIR', DATA_PATH . '/cer/');
