3e0c4c3a19738eadd5146896fd51067fa90d0d2e
carson <docxcn@gmail.com>
2015-10-13 11:16:04 +0800

    更新后台入口文件

--- admin/index.php
+++ admin/index.php
@@ -1,2 +1,15 @@
 <?php
-header('location: ../index.php?m=admin');
+
+/**
+ * ECTouch E-Commerce Project
+ * ============================================================================
+ * Copyright (c) 2014-2015 http://ectouch.cn All rights reserved.
+ * ----------------------------------------------------------------------------
+ * This is NOT a freeware, use is subject to license terms
+ * ----------------------------------------------------------------------------
+ * Licensed ( http://www.ectouch.cn/license )
+ * ----------------------------------------------------------------------------
+ */
+
+define('BIND_MODULE', 'Admin');
+require dirname(dirname(__FILE__)) . '/include/bootstrap.php';
