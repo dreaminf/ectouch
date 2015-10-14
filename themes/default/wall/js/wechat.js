2f2d23900c05e3df09e2ce90240472c754753a55
wanglu <wanglu@ecmoban.com>
2015-10-14 20:03:42 +0800

    优化留言墙修复留言墙显示优化留言墙完善留言墙样式

--- themes/default/wall/js/wechat.js
+++ themes/default/wall/js/wechat.js
@@ -2,7 +2,7 @@ $(function() {
 	$("#ul").niceScroll({
 		styler: "fb",
 		cursorcolor: "#39b4e9",
-		cursorwidth: '6',
+		cursorwidth: '0',
 		cursorborderradius: '10px',
 		background: '#000000',
 		spacebarenabled: false,
