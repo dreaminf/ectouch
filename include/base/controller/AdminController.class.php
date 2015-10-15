0b47098d4e092b0f829264490ec1f1b76686bb0b
carson <docxcn@gmail.com>
2015-10-15 16:22:19 +0800

    更新登录验证

--- include/base/controller/AdminController.class.php
+++ include/base/controller/AdminController.class.php
@@ -30,79 +30,13 @@ class AdminController extends BaseController {
     }
 
     protected function checkLogin() {
-        //不需要登录验证的页面
-        $access = array('Wechat', 'Extend', 'Upload', 'Authorization', 'Navigator', 'Index' => array('license', 'uploader'));
+        $access = array('wechat', 'extend', 'upload', 'authorization', 'navigator', 'index' => array('license', 'uploader'));
         
-        //如果当前访问是无需登录验证，则直接返回
-        if (!in_array(CONTROLLER_NAME, $access) && !in_array(ACTION_NAME, $access[CONTROLLER_NAME])) {
+        if (!in_array(strtolower(CONTROLLER_NAME), $access) || 
+            !in_array(strtolower(ACTION_NAME), $access[strtolower(CONTROLLER_NAME)]) || 
+            intval($_SESSION['admin_id']) <= 0) {
             $this->redirect('./admin');
         }
-
-        if (intval($_SESSION['admin_id']) > 0) {
-            return true;
-        }
-
-        //没有登录,则跳转到登录页面
-        if (!$this->isLogin()) {
-             $this->redirect(url('admin/index/login'));
-        }
-        return true;
-    }
-
-    //判断是否登录
-    protected function isLogin() {
-        if (empty($_SESSION[APP_NAME . '_USERINFO'])) {
-            /* session 不存在，检查cookie */
-            if (!empty($_COOKIE['ECTOUCHCP']['ADMIN_ID']) && !empty($_COOKIE['ECTOUCHCP']['ADMIN_PWD'])) {
-                // 找到了cookie, 验证cookie信息
-                $condition['user_id'] = intval($_COOKIE['ECTOUCHCP']['ADMIN_ID']);
-                $userInfo = $this->model->table('admin_user')->field('user_id, user_name, password, email, last_login, ec_salt')->where($condition)->find();
-                if (empty($userInfo)) {
-                    // 没有找到这个记录
-                    setcookie($_COOKIE['ECTOUCHCP']['ADMIN_ID'], '', 1);
-                    setcookie($_COOKIE['ECTOUCHCP']['ADMIN_PWD'], '', 1);
-                    return false;
-                } else {
-                    // 检查密码是否正确
-                    if (md5(md5($userInfo['user_id'] . $userInfo['user_name']) . C('hash_code')) == $_COOKIE['ECTOUCHCP']['ADMIN_PWD']) {
-                        $this->setLogin($userInfo);
-                        $data['last_login'] = gmtime();
-                        $data['last_ip'] = get_client_ip();
-                        $this->model->table('admin_user')->data($data)->where($condition)->update();
-                        $this->userInfo = $_SESSION[APP_NAME . '_USERINFO'];
-                        return true;
-                    } else {
-                        setcookie($_COOKIE['ECTOUCHCP']['ADMIN_ID'], '', 1);
-                        setcookie($_COOKIE['ECTOUCHCP']['ADMIN_PWD'], '', 1);
-                        return false;
-                    }
-                }
-            }
-            return false;
-        } else {
-            $this->userInfo = $_SESSION[APP_NAME . '_USERINFO'];
-            return true;
-        }
-    }
-
-    //设置登录
-    protected function setLogin($userInfo) {
-        $_SESSION[APP_NAME . '_USERINFO'] = $userInfo;
-    }
-
-    //退出登录
-    protected function clearLogin($url = '') {
-        //清除来路
-        unset($_SESSION['safe_route']);
-        /* 清除cookie */
-        setcookie('ECTOUCHCP[ADMIN_ID]', '', 1);
-        setcookie('ECTOUCHCP[ADMIN_PWD]', '', 1);
-        $_SESSION[APP_NAME . '_USERINFO'] = NULL;
-        if (!empty($url)) {
-            $_SESSION['safe_route'] = true;
-            $this->redirect($url);
-        }
-        return true;
     }
 
     //$upload_dir上传的目录名
