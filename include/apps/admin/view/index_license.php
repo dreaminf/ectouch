80a1cb2a057e1e0b6e8b18c0fa7dbc3d6bbc65f7
wangshuai <wangshuai@ecmoban.com>
2015-10-22 14:31:16 +0800

    修复授权码页面问题

--- include/apps/admin/view/index_license.php
+++ include/apps/admin/view/index_license.php
@@ -2,69 +2,65 @@
 {if $empower}
 
 
-    <style type="text/css">
-        <!--
-        body{ background:#fff; text-align:center; font-size:12px;}
-        a { color:#333; text-decoration:none;}
-        #cert{background:#fff url(http://www.phpcms.cn/statics/images/license/license_bg.jpg) no-repeat 0 0; width:632px; height:396px; margin:50px auto; position:relative;}
-        #cert table{ position:absolute; left:160px; top:130px;}
-        #cert table th{ height:30px; line-height:30px; text-align:right; padding-right:10px; font-weight:normal; color:#000;}
-        #cert table td{ text-align:left; color:#333;}
-        -->
-    </style>
+<style type="text/css">
+    body,p{margin:0;padding:0;border:0;}
+    ul{width:450px; position:absolute;left:325px; top:268px;overflow:hidden;}
+    ul li{list-style:none;float:left;}
+    p{line-height:32px;color:#414040;font-size:14px;}
+    .certificate{background-image:url(__PUBLIC__/images/license_bg.png);background-position:center;background-repeat:no-repeat;width:829px;height:640px;margin:0 auto;position:relative;}
+    .cont-title{text-align:right;}
+</style>
+<div class="certificate">
+    <ul>
+        <li>
+            <p class="cont-title">授权类型：<br>
+                授权域名：<br>
+                网站名称：<br>
+                授权持有人：<br>
+                授权起始时间：<br>
+                服务起止日期：
 
-<div id="cert">
-    <table cellpadding="0" cellspacing="0">
-        <tr>
-            <th>授权类型：</th>
-            <td>{$empower['type']}</td>
-        </tr>
-        <tr>
-            <th>授权域名：</th>
-            <td><a target="blank" href="{$empower['domain']}">{$empower['domain']}</a></td>
-        </tr>
-        <tr>
-            <th>网站名称：</th>
-            <td>{$empower['shop_name']}</td>
-        </tr>
-        <tr>
-            <th>授权持有人：</th>
-            <td>{$empower['name']}</td>
-        </tr>
-        <tr>
-            <th>授权起始时间：</th>
-            <td>{$empower['license_time']}</td>
-        </tr>
-        <tr>
-            <th>服务起止日期：</th>
-            <td>{$empower['service_time']}</td>
-        </tr>
-    </table>
+
+            </p>
+        </li>
+        <li>
+            <p>{$empower['type']}<br>
+                <a target="blank" href="{$empower['domain']}">{$empower['domain']}</a><br>
+                {$empower['shop_name']}<br>
+                {$empower['name']}<br>
+                {$empower['license_time']}<br>
+                {$empower['service_time']}
+
+            </p>
+
+        </li>
+    </ul>
+
 </div>
 {else}
 <div class="panel panel-default">
-  <div class="panel-heading">
-    <h3 class="panel-title">{$ur_here}</h3>
-  </div>
-  <div class="panel-body">
-    <form action="{url('license')}" method="post" enctype="multipart/form-data" class="form-horizontal" role="form">
-      <table id="general-table" class="table table-hover ectouch-table">
-        <tr>
-          <td width="200">授权序列号:</td>
-          <td><div class="col-md-4">
-              <input type='text' name='license' maxlength="20" id="a" class="form-control input-sm" />
-            </div></td>
-        </tr>
-        <tr>
-          <td></td>
-          <td><div class="col-md-4">
-              <input type="submit" value="{$lang['button_submit']}" class="btn btn-primary" />
-              <input type="reset" value="{$lang['button_reset']}" class="btn btn-default" />
-            </div></td>
-        </tr>
-      </table>
-    </form>
-  </div>
+    <div class="panel-heading">
+        <h3 class="panel-title">{$ur_here}</h3>
+    </div>
+    <div class="panel-body">
+        <form action="{url('license')}" method="post" enctype="multipart/form-data" class="form-horizontal" role="form">
+            <table id="general-table" class="table table-hover ectouch-table">
+                <tr>
+                    <td width="200">授权序列号:</td>
+                    <td><div class="col-md-4">
+                            <input type='text' name='license' maxlength="20" id="a" class="form-control input-sm" />
+                        </div></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td><div class="col-md-4">
+                            <input type="submit" value="{$lang['button_submit']}" class="btn btn-primary" />
+                            <input type="reset" value="{$lang['button_reset']}" class="btn btn-default" />
+                        </div></td>
+                </tr>
+            </table>
+        </form>
+    </div>
 </div>
 {/if}
 {include file="pagefooter"}
