4a891285fe3bf8d8ad7d9bb506d9e3e93e9c2240
wanglu <wanglu@ecmoban.com>
2015-10-23 18:20:46 +0800

    调试微信登陆

--- plugins/connect/weixin.php
+++ plugins/connect/weixin.php
@@ -107,15 +107,16 @@ class weixin {
     /**
      * 登录处理
      */
-    public function call_back($info, $url, $_GET, $type){
-        if (!empty($_GET['code'])) {
+    public function call_back($info, $url, $code, $type){
+        if (!empty($code)) {
+            $_GET['code'] = $code;
             $token = $this->weObj->getOauthAccessToken();
             $userinfo = $this->weObj->getOauthUserinfo($token['access_token'], $token['openid']);
             //公众号信息
             $wechat = model('Base')->model->table('wechat')->field('id, oauth_status')->where(array('type'=>2, 'status'=>1, 'default_wx'=>1))->find();
             $this->update_weixin_user($userinfo, $wechat['id'], $this->weObj);
             //用户是否绑定过
-            $isbind = model('Base')->model->table('wechat_user')->field('isbind')->where(array('openid'=>$userinfo['openid']))->getOne();
+            $isbind = $this->model->table('wechat_user')->field('isbind')->where(array('openid'=>$userinfo['openid']))->getOne();
             if($isbind || !$wechat['oauth_status']){
                 return true;
             }
