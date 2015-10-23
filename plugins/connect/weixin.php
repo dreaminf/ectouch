6d4b1d07ed741b0913176d069c45d8c47847d20b
wanglu <wanglu@ecmoban.com>
2015-10-23 16:47:55 +0800

    调试微信登陆

--- plugins/connect/weixin.php
+++ plugins/connect/weixin.php
@@ -110,6 +110,7 @@ class weixin {
     public function call_back($info, $url, $code, $type){
         if (!empty($code)) {
             $token = $this->weObj->getOauthAccessToken();
+            dump($token);
             $userinfo = $this->weObj->getOauthUserinfo($token['access_token'], $token['openid']);
             dump($userinfo);exit;
             //公众号信息
