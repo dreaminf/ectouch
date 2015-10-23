5a6fc396e4647e3a22b33773ebddca5e25e4a5ed
carson <docxcn@gmail.com>
2015-10-23 19:09:15 +0800

    发布2.0.7版本

--- data/version.php
+++ data/version.php
@@ -1,5 +1,5 @@
 <?php
 define('APPNAME', 'ECTouch');
-define('VERSION', '2.0.6');
-define('RELEASE', '20151016');
+define('VERSION', '2.0.7');
+define('RELEASE', '20151023');
 define('ECTOUCH_AUTH_KEY', '');
