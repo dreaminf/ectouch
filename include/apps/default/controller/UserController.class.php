2274549b463c56c687252a2fac38f80e5d11f5d2
wanglu <wanglu@ecmoban.com>
2015-10-30 18:09:48 +0800

    绑定后加上退出操作

--- include/apps/default/controller/UserController.class.php
+++ include/apps/default/controller/UserController.class.php
@@ -1809,6 +1809,7 @@ class UserController extends CommonController {
                     $user = model('Base')->model->table('wechat_user')->field('openid, ect_uid, isbind')->where($condition)->find();
                     if($user && empty($user['isbind'])){
                         model('Base')->model->table('wechat_user')->data(array('ect_uid'=>$user_id, 'isbind'=>1))->where($condition)->update();
+                        self::$user->logout();
                         show_message('账号绑定成功', '会员中心', url('index'), 'error');
                     }
                     else{
