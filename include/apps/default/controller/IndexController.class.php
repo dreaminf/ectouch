5f488aae8d0ffa2e1f5f91058428d3b83411659b
carson <docxcn@gmail.com>
2015-11-23 14:46:28 +0800

    首页增加缓存

--- include/apps/default/controller/IndexController.class.php
+++ include/apps/default/controller/IndexController.class.php
@@ -21,30 +21,34 @@ class IndexController extends CommonController {
      * 首页信息
      */
     public function index() {
-        // 自定义导航栏
-        $navigator = model('Common')->get_navigator();
-        $this->assign('navigator', $navigator['middle']);
-        $this->assign('best_goods', model('Index')->goods_list('best', C('page_size')));
-        $this->assign('new_goods', model('Index')->goods_list('new', C('page_size')));
-        $this->assign('hot_goods', model('Index')->goods_list('hot', C('page_size')));
-        // 调用促销商品
-        $this->assign('promotion_goods', model('Index')->goods_list('promotion', C('page_size')));
-        //首页推荐分类
-        $cat_rec = model('Index')->get_recommend_res(10,4);
-        $this->assign('cat_best', $cat_rec[1]);
-        $this->assign('cat_new', $cat_rec[2]);
-        $this->assign('cat_hot', $cat_rec[3]);
-        // 促销活动
-        $this->assign('promotion_info', model('GoodsBase')->get_promotion_info());
-        // 团购商品
-        $this->assign('group_buy_goods', model('Groupbuy')->group_buy_list(C('page_size'),1,'goods_id','ASC'));
-        // 获取分类
-        $this->assign('categories', model('CategoryBase')->get_categories_tree());
-        // 获取品牌
-        $this->assign('brand_list', model('Brand')->get_brands($app = 'brand', C('page_size'), 1));
-        // 分类下的文章
-        $this->assign('cat_articles', model('Article')->assign_articles(1,5)); // 1 是文章分类id ,5 是文章显示数量
-        $this->display('index.dwt');
+        $cache_id = sprintf('%X', crc32($_SESSION['user_rank'] . '-' . C('lang')));
+        if (!$this->is_cached('index.dwt', $cache_id))
+        {
+            // 自定义导航栏
+            $navigator = model('Common')->get_navigator();
+            $this->assign('navigator', $navigator['middle']);
+            $this->assign('best_goods', model('Index')->goods_list('best', C('page_size')));
+            $this->assign('new_goods', model('Index')->goods_list('new', C('page_size')));
+            $this->assign('hot_goods', model('Index')->goods_list('hot', C('page_size')));
+            // 调用促销商品
+            $this->assign('promotion_goods', model('Index')->goods_list('promotion', C('page_size')));
+            //首页推荐分类
+            $cat_rec = model('Index')->get_recommend_res(10,4);
+            $this->assign('cat_best', $cat_rec[1]);
+            $this->assign('cat_new', $cat_rec[2]);
+            $this->assign('cat_hot', $cat_rec[3]);
+            // 促销活动
+            $this->assign('promotion_info', model('GoodsBase')->get_promotion_info());
+            // 团购商品
+            $this->assign('group_buy_goods', model('Groupbuy')->group_buy_list(C('page_size'),1,'goods_id','ASC'));
+            // 获取分类
+            $this->assign('categories', model('CategoryBase')->get_categories_tree());
+            // 获取品牌
+            $this->assign('brand_list', model('Brand')->get_brands($app = 'brand', C('page_size'), 1));
+            // 分类下的文章
+            $this->assign('cat_articles', model('Article')->assign_articles(1,5)); // 1 是文章分类id ,5 是文章显示数量
+        }
+        $this->display('index.dwt', $cache_id);
     }
 
     /**
