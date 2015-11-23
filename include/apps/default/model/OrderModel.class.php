ab15739446c74f62fecb1678ce094eb57bf22d0c
carson <docxcn@gmail.com>
2015-11-23 14:36:00 +0800

    更新购物车商品显示

--- include/apps/default/model/OrderModel.class.php
+++ include/apps/default/model/OrderModel.class.php
@@ -926,14 +926,14 @@ class OrderModel extends BaseModel {
             'goods_amount' => 0, // 本店售价合计（无格式）
             'total_number' => 0,
         );
-
         /* 循环、统计 */
+        //购物车和PC端同步，没有登录SESS_ID验证
+        $SESS_ID = $_SESSION['user_id'] > 0 ? $_SESSION['user_id'] : SESS_ID;
         $sql = "SELECT *, IF(parent_id, parent_id, goods_id) AS pid " .
-                " FROM " . $this->pre . "cart " .
-                " WHERE session_id = '" . SESS_ID . "' AND rec_type = '" . CART_GENERAL_GOODS . "'" .
-                " ORDER BY pid, parent_id";
+            " FROM " . $this->pre . "cart " .
+            " WHERE user_id = '" . $SESS_ID . "' AND rec_type = '" . CART_GENERAL_GOODS . "'" .
+            " ORDER BY pid, parent_id";
         $res = $this->query($sql);
-
         /* 用于统计购物车中实体商品和虚拟商品的个数 */
         $virtual_goods_count = 0;
         $real_goods_count = 0;
