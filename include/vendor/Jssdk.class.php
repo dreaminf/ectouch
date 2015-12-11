90a2fdbc9efa10b084ce36a15bdb7990fe73da25
carson <docxcn@gmail.com>
2015-12-11 15:32:03 +0800

    将URL转换成小写

--- include/vendor/Jssdk.class.php
+++ include/vendor/Jssdk.class.php
@@ -14,7 +14,7 @@ class Jssdk {
 
     // 注意 URL 一定要动态获取，不能 hardcode.
     $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
-    $url = "$protocol$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+    $url = strtolower("$protocol$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]");
 
     $timestamp = time();
     $nonceStr = $this->createNonceStr();
