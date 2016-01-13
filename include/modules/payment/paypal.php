f25990e36a933175faf3ebcc49f959126adc6379
carson <docxcn@gmail.com>
2016-01-13 19:26:39 +0800

    更新paypal支付插件

--- include/modules/payment/paypal.php
+++ include/modules/payment/paypal.php
@@ -8,28 +8,25 @@ if (file_exists($payment_lang)) {
     include_once($payment_lang);
 }
 
-/* 模块的基本信息 */
 if (isset($set_modules) && $set_modules == TRUE)
 {
     $i = isset($modules) ? count($modules) : 0;
-    /* 代码 */
+
     $modules[$i]['code']    = basename(__FILE__, '.php');
-    /* 描述对应的语言项 */
     $modules[$i]['desc']    = 'paypal_desc';
-    /* 是否支持货到付款 */
     $modules[$i]['is_cod']  = '0';
-    /* 是否支持在线支付 */
     $modules[$i]['is_online']  = '1';
-    /* 作者 */
-    $modules[$i]['author']  = 'ECTouch Team';
-    /* 网址 */
+    $modules[$i]['author']  = 'ECTouch';
     $modules[$i]['website'] = 'http://www.paypal.com';
-    /* 版本号 */
-    $modules[$i]['version'] = '1.0.0';
-    /* 配置信息 */
+    $modules[$i]['version'] = '2.0.0';
     $modules[$i]['config'] = array(
         array('name' => 'paypal_account', 'type' => 'text', 'value' => ''),
+        array('name' => 'paypal_username', 'type' => 'text', 'value' => ''),
+        array('name' => 'paypal_password', 'type' => 'text', 'value' => ''),
+        array('name' => 'paypal_signature', 'type' => 'text', 'value' => ''),
+        array('name' => 'paypal_sandbox', 'type' => 'select', 'value' => '1'),
         array('name' => 'paypal_currency', 'type' => 'select', 'value' => 'USD')
     );
+
     return;
 }
