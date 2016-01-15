101fb2df9c9f2fba2b1543654556438f032a2dc6
changye@ecmoban.com <changye@ecmoban.com>
2016-01-15 18:49:55 +0800

    新增编辑文档页面，首页内容菜单栏点击问题，修复收缩框异常，翻页效果

--- themes/html/admin/js/comm.js
+++ themes/html/admin/js/comm.js
@@ -1,8 +1,4 @@
 $(function($) {
-	$('.nav-list li').click(function() {
-		$(this).toggleClass('active').siblings().removeClass('active');
-
-	});
 	var header = $('.main-left').outerWidth();
 	$('.sidemenu-header').click(function() {
 		$('.main-left').toggleClass('active');
@@ -16,17 +12,18 @@ $(function($) {
 	});
 	$(".nav-list").collapse("show");
 	//tabmenu
-	$('.main-sidemenu-list li').click(function() {
-		$('.main-content-content').hide().eq($('.main-sidemenu-list li').index(this)).show();
+	$('.main-sidemenu-cont li').bind('click',function() {
+		$('.main-sidemenu-cont li').removeClass('active');
+		$(this).addClass('active');
 	})
 	$(".main-collapse").click(function() {
 		$('.main-content-sidemenu').toggleClass("active");
 		$('.main-content-content').toggleClass("select");
 		$(this).toggleClass("active");
+		//$('.layer-shade').toggleClass('active');
 	});
 	//cookie
 	//menu-state
-	//	var header = $('.main-left').outerWidth();
 	if ($.cookie("menuheader") != undefined) {
 		//cookie记录的index
 		if ($.cookie("menuheader") <= 50) {
@@ -36,6 +33,10 @@ $(function($) {
 	} else {
 		$.cookie("menuheader", header);
 	}
+	//pagination
+	$('.pagination li').click(function () {
+		$(this).addClass('active').siblings().removeClass('active');
+	})
 	//menu-state
 	$('.main-left').find('.nav-list li').click(function() {
 		$('.main-left').find('.nav-list li').removeClass('active');
@@ -48,15 +49,21 @@ $(function($) {
 		var num = $.cookie("current");
 		$('.main-left').find('.nav-list li').eq(num).addClass('active').siblings().removeClass('active');
 	}
-	
+	//编辑
 	$('.edit').click(function() {
-		$('.layer-content').show();
-	});
-	$('.layer-close').click(function() {
-		$('.layer-content').hide();
+		//$('.layer-shade').show();
+		layer.open({
+			type: 2,
+			title: '内容编辑',
+			shadeClose: true,
+			shade: 0,
+			maxmin: true,
+			area: ['893px', '600px'],
+			content: 'comm-edit.html'
+		});
 	});
 	//datapicker
-	$('.date').cxCalendar({
+	$('.date ').cxCalendar({
 		type: 'datetime',
 		format: 'YYYY-MM-DD HH:mm:ss',
 	});
