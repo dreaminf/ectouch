12e7ed3458d54734c036b8133ff1c9630c1bbed0
carson <wanganlin@ecmoban.com>
2016-03-15 20:27:29 +0800

    优化关注

--- include/apps/default/controllers/CommonController.class.php
+++ include/apps/default/controllers/CommonController.class.php
@@ -43,13 +43,12 @@ class CommonController extends BaseController
         }
         if(is_wechat_browser()){
             //是否显示关注按钮
-            $condition['openid'] = !empty($_SESSION['openid']) ? $_SESSION['openid'] : 0;       
-            $userinfo = $this->model->table('wechat_user')->field('subscribe')->where($condition)->find();
-            $this->subscribe = $userinfo['subscribe'];
-            $subscribe_qrcode = url('api/qrcode', array('username'=> $_SESSION['username'], 'userid'=> $_SESSION['userid']));
-            exit($subscribe_qrcode);
-            $this->assign('subscribe_qrcode', $subscribe_qrcode);
-            $this->assign('subscribe', $userinfo['subscribe']);
+            $condition['openid'] = !empty($_SESSION['openid']) ? $_SESSION['openid'] : 0;
+            $_SESSION['subscribe'] = $this->model->table('wechat_user')->field('subscribe')->where($condition)->find();
+            if(!$this->subscribe){
+                $subscribe = url('api/qrcode', array('username'=> $_SESSION['user_name'], 'userid'=> $_SESSION['user_id']));
+            }
+            $this->assign('subscribe', $subscribe);
             // 设置默认分享图片
             $share_img = '<div style="margin:0 auto;width:0px;height:0px;overflow:hidden;"><img src="__TPL__/images/share.png"></div>';
             $this->assign('share_img', $share_img);
