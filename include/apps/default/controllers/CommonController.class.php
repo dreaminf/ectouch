cf26cf06f6ad1299b5ac90a12938d6419df59599
carson <wanganlin@ecmoban.com>
2016-03-15 21:17:53 +0800

    test

--- include/apps/default/controllers/CommonController.class.php
+++ include/apps/default/controllers/CommonController.class.php
@@ -44,11 +44,9 @@ class CommonController extends BaseController
         if(is_wechat_browser()){
             //是否显示关注按钮
             $condition['openid'] = !empty($_SESSION['openid']) ? $_SESSION['openid'] : 0;
-            $_SESSION['subscribe'] = $this->model->table('wechat_user')->field('subscribe')->where($condition)->find();
-            if(!$_SESSION['subscribe']){
-                $subscribe = 1;
-            }
-            $this->assign('subscribe', 1);
+            $userinfo = $this->model->table('wechat_user')->field('subscribe')->where($condition)->find();
+            $_SESSION['subscribe'] = $userinfo['subscribe'];
+            $this->assign('subscribe', $userinfo['subscribe']);
             // 设置默认分享图片
             $share_img = '<div style="margin:0 auto;width:0px;height:0px;overflow:hidden;"><img src="__TPL__/images/share.png"></div>';
             $this->assign('share_img', $share_img);
