5037648b1bc987f2e1711863abeaae41b7bb215c
hanbenfa <hanbenfa@ecmoban.com>
2016-03-16 16:07:57 +0800

    修复店铺权限

--- include/apps/default/controllers/StoreController.class.php
+++ include/apps/default/controllers/StoreController.class.php
@@ -49,7 +49,7 @@ class StoreController extends CommonController {
      */
     public function check(){
 		$action = ACTION_NAME;
-        $drp_show = $this->model->table('drp_shop')->where(array('id'=>$_GET['drp_id']))->field('id, open, audit')->find();
+        $drp_show = $this->model->table('drp_shop')->where(array('user_id'=>$_SESSION['user_id']))->field('id, open, audit')->find();
         if($action == 'index' && $drp_show['audit'] != 1){
             show_message('请等待管理员审核','进入商城',url('index/index'));
         }
