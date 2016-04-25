e07b8924f87fd6594595c4adf1a1e1f8171d55bd
carson <wanganlih@ecmoban.com>
2016-04-25 14:01:23 +0800

    更新手机号码格式

--- include/apps/common/helpers/function.php
+++ include/apps/common/helpers/function.php
@@ -1094,7 +1094,7 @@ function is_email($user_email) {
  * @return bool
  */
 function is_mobile($user_email) {
-    $chars = "/^13[0-9]{1}[0-9]{8}$|15[0-9]{1}[0-9]{8}$|18[0-9]{1}[0-9]{8}$/";
+    $chars = '/^13[0-9]{9}|15[012356789][0-9]{8}|18[0-9]{9}|14[579][0-9]{8}|17[0-9]{9}$/';
     if (preg_match($chars, $user_email)) {
         return true;
     } else {
