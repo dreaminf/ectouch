14599fb3beef1ef67a2605d9cb10009a9ea178a3
carson <docxcn@gmail.com>
2016-05-26 14:54:23 +0800

    增加安装和更新测试程序

---
+++ test_upgrade/packages/v1.3.5/upgrade.php
@@ -0,0 +1,19 @@
+<?php
+class upgrade
+{
+    public $sql_files = array(
+        'structure' => 'structure.sql',
+        'data' => 'data.sql'
+    );
+
+    function update_database_optionally()
+    {
+
+    }
+
+    function update_files()
+    {
+
+    }
+
+}
