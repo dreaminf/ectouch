c793784934a3bfae93adcb4c3c836c2240a7d23a
xiaorui <xiaorui@ecmoban.com>
2016-06-30 10:51:59 +0800

    修改数据迁移

--- data/migrates/migrate-201606291600.sql
+++ data/migrates/migrate-201606291600.sql
@@ -3,9 +3,6 @@
 -- date:20160629
 -- description:新增自由组合插件的数据表
 -- ------------------------------------------------------------
-ALTER TABLE `ecs_group_goods` ADD COLUMN `group_id` tinyint(3) UNSIGNED NOT NULL DEFAULT 0 COMMENT '配件分组' AFTER `admin_id`;
-
-ALTER TABLE `ecs_cart` ADD COLUMN `group_id` varchar(255) NOT NULL DEFAULT ‘’ COMMENT '套餐组合分组标识' AFTER `goods_attr_id`;
-
+ALTER TABLE `ecs_group_goods` ADD `group_id` tinyint(3) UNSIGNED NOT NULL AFTER `admin_id`;
+ALTER TABLE `ecs_cart` ADD `group_id` varchar(255) NOT NULL AFTER `goods_attr_id`;
 CREATE TABLE ecs_cart_combo like  ecs_cart;
-
