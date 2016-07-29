67bf0444d388cfcbdeeaddc79f36e6f359129bec
carson <docxcn@gmail.com>
2016-07-29 18:23:11 +0800

    移除重复的函数

--- include/helpers/main_helper.php
+++ include/helpers/main_helper.php
@@ -2072,57 +2072,6 @@ function url_domain()
 
     return $root;
 }
-/**
- * 显示一个提示信息 by ECTocuh Leah
- *
- * @access  public
- * @param   string  $content
- * @param   string  $link
- * @param   string  $href
- * @param   string  $type               信息类型：warning, error, info
- * @param   string  $auto_redirect      是否自动跳转
- * @return  void
- */
-function show_service_message($content, $links = '', $hrefs = '', $type = 'info', $auto_redirect = true)
-{
-    assign_template();
-
-    $msg['content'] = $content;
-    if (is_array($links) && is_array($hrefs))
-    {
-        if (!empty($links) && count($links) == count($hrefs))
-        {
-            foreach($links as $key =>$val)
-            {
-                $msg['url_info'][$val] = $hrefs[$key];
-            }
-            $msg['back_url'] = $hrefs['0'];
-        }
-    }
-    else
-    {
-        $link   = empty($links) ? $GLOBALS['_LANG']['back_up_page'] : $links;
-        $href    = empty($hrefs) ? 'javascript:history.back()'       : $hrefs;
-        $msg['url_info'][$link] = $href;
-        $msg['back_url'] = $href;
-    }
-
-    $msg['type']    = $type;
-    $position = assign_ur_here(0, $GLOBALS['_LANG']['sys_msg']);
-    $GLOBALS['smarty']->assign('page_title', $position['title']);   // 页面标题
-    $GLOBALS['smarty']->assign('ur_here',    $position['ur_here']); // 当前位置
-
-    if (is_null($GLOBALS['smarty']->get_template_vars('helps')))
-    {
-        $GLOBALS['smarty']->assign('helps', get_shop_help()); // 网店帮助
-    }
-    $GLOBALS['smarty']->assign('auto_redirect', $auto_redirect);
-    $GLOBALS['smarty']->assign('message', $msg);
-    $GLOBALS['smarty']->display('service_message.dwt');
-
-    exit;
-}
-
 
 /**
  * 显示一个提示信息 by ECTocuh Leah
@@ -2174,4 +2123,3 @@ function show_service_message($content, $links = '', $hrefs = '', $type = 'info'
 
     exit;
 }
-
