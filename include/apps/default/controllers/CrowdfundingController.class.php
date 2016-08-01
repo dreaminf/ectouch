a1db0f7e1ecab53fb5afe1670c00c7105b94bcd3
hanbenfa@ecmoban.com <hanbenfa@ecmoban.com>
2016-08-01 15:44:39 +0800

    上传众筹购物流程模块

---
+++ include/apps/default/controllers/CrowdfundingController.class.php
@@ -0,0 +1,275 @@
+<?php
+
+/**
+ * ECTouch Open Source Project
+ * ============================================================================
+ * Copyright (c) 2012-2014 http://ectouch.cn All rights reserved.
+ * ----------------------------------------------------------------------------
+ * 文件名称：IndexController.class.php
+ * ----------------------------------------------------------------------------
+ * 功能描述：ECTouch众筹分类商品列表控制器
+ * ----------------------------------------------------------------------------
+ * Licensed ( http://www.ectouch.cn/docs/license.txt )
+ * ----------------------------------------------------------------------------
+ */
+/* 访问控制 */
+defined('IN_ECTOUCH') or die('Deny Access');
+
+class CrowdfundingController extends CommonController {
+	 /**
+     * 构造函数
+     */
+    public function __construct()
+    {
+        parent::__construct();
+		$this->user_id = $_SESSION['user_id'];
+        $this->cat_id = I('request.id');
+		$this->type = I('request.type');
+		$this->keywords = I('request.keywords');
+		$this->goods_id = I('request.id');
+    }
+	
+    /**
+     * 众筹项目列表信息
+     */
+    public function index() {
+        $category = model('Crowdfunding')->category_all();//获取众筹分类
+		$goodslist = $this->crowd_goods();//获取众筹产品列表
+		if($this->cat_id){
+			$this->assign('id', $this->cat_id);			
+		}
+		$this->assign('type', $this->type);
+		$this->assign('goods_list', $goodslist);
+		$this->assign('category', $category);
+        $this->display('crowd/raise_category.html');
+    }
+	
+    /**
+     * 众筹项目列表信息
+     */
+	public function crowd_goods() {
+		if ($this->keywords) {
+            $where .= " and goods_name like '%$this->keywords%' ";
+        }		
+		if ($this->cat_id > 0) {
+            $where .= " and cat_id = $this->cat_id ";
+        }	
+		
+		if ($this->type) {
+            switch ($this->type) {
+                case 'new':
+                    $where .= ' order by start_time DESC';
+                    break;
+                case 'sum_price':
+                    $where .= ' order by sum_price DESC ';
+                    break;
+                case 'buy_num':
+                    $where .= ' order by buy_num DESC ';
+                    break;
+                default:
+                    $where .= '';
+            }
+        }
+		$sql = 'SELECT goods_id, cat_id, goods_name, goods_img, sum_price, total_price, start_time, time, like_num, buy_num '.'FROM '
+		. $this->model->pre . 'crowd_goods ' . "WHERE is_verify = 1 $where ";
+        $res = $this->model->query($sql);
+		$goods = array();
+        foreach ($res AS $key => $row) {
+            $goods[$key]['id'] = $row['goods_id'];
+			$goods[$key]['cat_id'] = $row['cat_id'];
+            $goods[$key]['goods_name'] = $row['goods_name'];
+            $goods[$key]['like_num'] = $row['like_num'];
+            $goods[$key]['buy_num'] = $row['buy_num'];
+			$goods[$key]['start_time'] =floor((gmtime()-$row['start_time'])/86400);
+            $goods[$key]['sum_price'] = price_format($row['sum_price']);
+            $goods[$key]['total_price'] = price_format($row['total_price']);
+            $goods[$key]['goods_img'] = 'data/attached/crowdimage/'.$row['goods_img'];
+            $goods[$key]['url'] = url('Crowdfunding/goods_info', array('id' => $row['goods_id']));
+			$goods[$key]['bar'] = $row['total_price']*100/$row['sum_price'];
+			$goods[$key]['bar'] = round($goods[$key]['bar'],1); //计算百分比
+        }
+        return $goods;
+		
+	}
+	
+	
+	/**
+     * 众筹项目详情信息
+     */
+	public function goods_info() {
+
+		$goods = model('Crowdfunding')->crowd_goods_info($this->goods_id);  //项目信息
+		//项目相册
+		$gallery = explode(',',$goods['gallery_img']);
+		foreach ($gallery as $key=>$val) {
+			$gallery_img[$key] = 'data/attached/crowdimage/'.$val;
+        }
+		$this->assign('gallery_img', $gallery_img);
+		$goods_plan = model('Crowdfunding')->crowd_goods_paln($this->goods_id);//项目方案
+		$comment_list = model('Crowdfunding')->crowd_comment($this->goods_id);//项目评论
+		$trends_list = model('Crowdfunding')->crowd_trends($this->goods_id);//项目评论
+		$buy_list = model('Crowdfunding')->crowd_buy($this->goods_id);//项目的支持者
+		// 检查是否已经存在于用户的关注列表
+        if ($_SESSION ['user_id']) {
+            $where['user_id'] = $_SESSION ['user_id'];
+            $where['goods_id'] = $this->goods_id;
+            $rs = $this->model->table('crowd_like')->where($where)->count();
+            if ($rs > 0) {
+                $this->assign('sc', 1);
+            }
+        }
+		$this->assign('goods', $goods);
+		$this->assign('id', $this->goods_id);
+		$this->assign('goods_plan', $goods_plan);
+		$this->assign('comment_list', $comment_list);
+		$this->assign('trends_list', $trends_list);
+		$this->assign('buy_list', $buy_list);
+		$this->display('crowd/raise_goods.html');
+	}
+	
+	/**
+     * 改变方案数量修改价格
+     */
+	public function plan_price() {
+
+		//格式化返回数组
+        $res = array(
+            'err_msg' => '',
+            'result' => '',
+            'qty' => 1
+        );
+        // 获取参数
+		$goods_id = (isset($_REQUEST ['goods_id'])) ? intval($_REQUEST ['goods_id']) : 1;
+		$cp_id = (isset($_REQUEST ['cp_id'])) ? intval($_REQUEST ['cp_id']) : 1;
+        $number = (isset($_REQUEST ['number'])) ? intval($_REQUEST ['number']) : 1;
+        // 如果商品id错误
+        if ($goods_id == 0) {
+            $res ['err_msg'] = L('err_change_attr');
+            $res ['err_no'] = 1;
+        } else {
+            // 查询
+            $condition['goods_id'] = $goods_id;
+			$condition['cp_id'] = $cp_id;
+            $goods = $this->model->table('crowd_plan')->field('shop_price , number ,backey_num')->where($condition)->find();
+			$surplus_num = $goods['number'] - $goods['backey_num'];
+            if ($number <= 0) {
+                $res ['qty'] = $number = 1;
+            } else {
+                $res ['qty'] = $number;
+            }
+			if($number > $surplus_num){
+				$res ['err_msg'] = '已超出计划销售数量';
+				$res ['err_no'] = 1;
+				
+			}
+            $res ['result'] = price_format($goods['shop_price'] * $number);
+        }
+        die(json_encode($res));
+	}
+	
+	/**
+     * 加入关注
+     */
+	public function add_crowd_like(){
+		
+		$result = array(
+            'error' => 0,
+            'message' => ''
+        );
+        $goods_id = intval($_GET['id']);
+      
+		// 检查是否已经存在于用户的关注列表
+		$where['user_id'] = $this->user_id;
+		$where['goods_id'] = $goods_id;
+		$rs = $this->model->table('crowd_like')
+				->where($where)
+				->count();
+		if ($rs > 0) {
+			$rs = $this->model->table('crowd_like')
+					->where($where)
+					->delete();
+			if (!$rs) {
+				$result['error'] = 1;
+				$result['message'] = M()->errorMsg();
+				die(json_encode($result));
+			} else {
+				$result['error'] = 0;
+				$result['message'] = '已成移除加关注列表';
+				die(json_encode($result));
+			}
+		} else {
+			$data['user_id'] = $this->user_id;
+			$data['goods_id'] = $goods_id;
+			$data['add_time'] = gmtime();
+			if ($this->model->table('crowd_like')
+							->data($data)
+							->insert() === false) {
+				$result['error'] = 1;
+				$result['message'] = M()->errorMsg();
+				die(json_encode($result));
+			} else {
+				$result['error'] = 0;
+				$result['message'] = '已成功添加关注列表';
+				die(json_encode($result));
+			}
+		}
+        
+		
+	}
+	
+	/**
+     * 获取众筹项目的评论列表
+     */
+	public function crowd_comment_list(){
+		
+        $cmt->id = !empty($_GET['id']) ? intval($_GET['id']) : 0;
+        $cmt->type = !empty($_GET['type']) ? intval($_GET['type']) : 1;
+        $cmt->page = isset($_GET['page']) && intval($_GET['page']) > 0 ? intval($_GET['page']) : 1;
+        $com = model('Crowdfunding')->crowd_comment_info($cmt->id,$cmt->type);
+        $this->assign('comments_info', $com);
+        $pay = 0;
+        $size = I(C('page_size'), 10);
+        $this->assign('show_asynclist', C('show_asynclist'));
+        $count = $com['sum_count'];
+        $filter['page'] = '{page}';
+        $offset = $this->pageLimit(url('Crowdfunding/crowd_comment_list', $filter), $size);
+        $offset_page = explode(',', $offset);
+        $comment_list = model('Crowdfunding')->crowd_get_comment($cmt->id, $cmt->type, $pay, $offset_page[1], $offset_page[0]);
+        $this->assign('comment_list', $comment_list);
+     
+        $result['message'] = C('comment_check') ? L('cmt_submit_wait') : L('cmt_submit_done');
+        $this->assign('id', $cmt->id);
+        $this->assign('type', $cmt->type);
+        $this->assign('pager', $this->pageShow($count));
+        $this->assign('title', L('goods_comment'));
+		
+		$this->display('crowd/raise_goods_evaluation.html');
+	}
+	
+	
+	/**
+     * 获取众筹项目的详情
+     */
+	public function crowd_goods_properties(){
+		$goods_desc = $this->model->table('crowd_goods')->where(array('goods_id'=>$this->goods_id))->field('goods_desc,goods_id')->find();
+		$this->assign('goods', $goods_desc);
+		$this->display('crowd/raise_goods-info.html');
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+   
+
+}
---
+++ include/apps/default/models/CrowdfundingModel.class.php
@@ -0,0 +1,362 @@
+<?php
+
+/**
+ * ECTouch Open Source Project
+ * ============================================================================
+ * Copyright (c) 2012-2014 http://ectouch.cn All rights reserved.
+ * ----------------------------------------------------------------------------
+ * 文件名称：IndexModel.class.php
+ * ----------------------------------------------------------------------------
+ * 功能描述：ECTOUCH 众筹分类商品列表
+ * ----------------------------------------------------------------------------
+ * Licensed ( http://www.ectouch.cn/docs/license.txt )
+ * ----------------------------------------------------------------------------
+ */
+/* 访问控制 */
+defined('IN_ECTOUCH') or die('Deny Access');
+
+class CrowdfundingModel extends CommonModel {
+	/**
+     * 获取当前分类及其子分类 
+     * @param  $type
+     * @param  $limit
+     * @param  $start
+     */
+	function category_all(){		
+		/* 获取当前分类及其子分类 */
+		 $sql = 'SELECT c.cat_id,c.cat_name,c.parent_id,c.is_show ' .
+				'FROM ' . $this->pre . 'crowd_category as c ' .
+				"WHERE c.parent_id = 0 AND c.is_show = 1 ORDER BY c.sort_order ASC, c.cat_id ASC";
+
+		$res = $this->query($sql);
+		foreach ($res AS $row) {
+			if ($row['is_show']) {
+				$cat_arr[$row['cat_id']]['id'] = $row['cat_id'];
+				$cat_arr[$row['cat_id']]['name'] = $row['cat_name'];
+				if (isset($row['cat_id']) == isset($row['parent_id'])) {
+					$cat_arr[$row['cat_id']]['cat_id'] = $this->get_child_trees($row['cat_id']);
+				}
+			}
+		}
+
+		return $cat_arr;
+	}
+	
+	/* 获取当前分类及其子分类 */
+	function get_child_trees($tree_id = 0) {
+		$three_arr = array();
+		$sql = 'SELECT count(*) FROM '. $this->pre . "crowd_category WHERE parent_id = '$tree_id' AND is_show = 1 ";
+		if ($this->row($sql) || $tree_id == 0) {
+			$child_sql = 'SELECT c.cat_id, c.cat_name, c.parent_id, c.is_show ' .
+					'FROM ' . $this->pre . 'crowd_category as c ' .
+					" WHERE c.parent_id = '$tree_id' AND c.is_show = 1 GROUP BY c.cat_id ORDER BY c.sort_order ASC, c.cat_id ASC";
+			$res = $this->query($child_sql);
+			foreach ($res AS $row) {
+				if ($row['is_show']) {
+					$three_arr[$row['cat_id']]['id'] = $row['cat_id'];
+					$three_arr[$row['cat_id']]['name'] = $row['cat_name'];
+				}
+				if (isset($row['cat_id']) != NULL) {
+					$three_arr[$row['cat_id']]['cat_id'] = $this->get_child_trees($row['cat_id']);
+				}
+			}
+		}
+		return $three_arr;
+	}
+	/* 获取当前项目信息 */
+	function crowd_goods_info($goods_id = 0) {
+		$sql = 'SELECT goods_id, cat_id, goods_name, goods_img, sum_price, total_price, start_time,time,shiping_time,gallery_img, like_num, buy_num '.'FROM '. $this->pre . 'crowd_goods ' . "WHERE is_verify = 1 and goods_id = '$goods_id' ";
+        $row = $this->row($sql);
+        if ($row !== false) {
+            $row['goods_id'] = $row['goods_id'];
+			$row['cat_id'] = $row['cat_id'];
+            $row['goods_name'] = $row['goods_name'];
+            $row['like_num'] = $row['like_num'];
+			$row['gallery_img'] = $row['gallery_img'];
+            $row['buy_num'] = $this->crowd_buy_num($row['goods_id']);
+			$row['start_time'] =floor((gmtime()-$row['start_time'])/86400);
+			$row['time'] = $row['time'] - $row['start_time'];
+			$row['shiping_time'] = local_date(C('time_format'), $row['shiping_time']);
+			$row['sum_price'] = $row['sum_price'];
+			$row['total_price'] = $row['total_price'];
+            $row['goods_img'] = 'data/attached/crowdimage/'.$row['goods_img'];
+            $row['url'] = url('Crowdfunding/goods_info', array('id' => $row['goods_id']));
+			$row['bar'] = $row['total_price']*100/$row['sum_price'];
+			$row['bar'] = round($row['bar'],1); //计算百分比
+
+            return $row;
+        } else {
+            return false;
+        }
+		
+		
+	}
+	
+	/* 获取当前项目购买人数量 */
+	function crowd_buy_num($goods_id = 0) {
+		$sql = "SELECT count(rec_id) as num  FROM ". $this->pre ."order_info as o left join  ". $this->pre ."order_goods as g on o.order_id = g.order_id". " WHERE g.goods_id = '".$goods_id."' AND o.extension_code = 'crowd_buy' ";
+        $res = $this->row($sql);
+		return $res['num'];
+	}
+	
+	
+	
+	/* 获取当前项目方案 */
+	function crowd_goods_paln($goods_id = 0) {
+		$sql = 'SELECT cp_id, name, goods_id, shop_price, number, backey_num, cp_img, return_time '.'FROM '
+		. $this->model->pre . 'crowd_plan ' . "WHERE status = 1 and goods_id = '$goods_id' order by sort_order ASC ";
+        $res = $this->query($sql);
+		
+		$plan = array();
+        foreach ($res AS $key => $row) {
+            $plan[$key]['cp_id'] = $row['cp_id'];
+			$plan[$key]['name'] = $row['name'];
+            $plan[$key]['goods_id'] = $row['goods_id'];
+			$plan[$key]['shop_price'] = price_format($row['shop_price']);
+            $plan[$key]['number'] = $row['number'];
+            $plan[$key]['backey_num'] = $row['backey_num'];
+			$plan[$key]['surplus_num'] = $row['number']-$row['backey_num'];
+            $plan[$key]['cp_img'] = 'data/attached/crowdimage/'.$row['cp_img'];           
+        }
+        return $plan;
+		
+	}
+	
+	/* 获取当前项目评论 */
+	function crowd_comment($goods_id = 0) {
+		$sql = "SELECT id,user_id,user_name,goods_id,content,add_time FROM ". $this->model->pre ."crowd_comment WHERE ".
+               "goods_id = '".$goods_id."' AND status = 1 and parent_id = 0  ORDER BY id DESC LIMIT 0,5";
+        $comment_list = $this->model->query($sql);
+        foreach($comment_list as $k => $v){
+			$comment_list[$k]['add_time'] = local_date(C('time_format'), $v['add_time']);
+			$comment_list[$k]['name'] =  $v['user_name'];
+			$comment_list[$k]['avatar'] = '';
+			$wechat_user = $this->model->table('wechat_user')->where("ect_uid=".$v['user_id'])->field('nickname,headimgurl')->find();
+			if(!empty($wechat_user)){
+				$comment_list[$k]['name'] =  $wechat_user['nickname'];
+				$comment_list[$k]['avatar'] = $wechat_user['headimgurl'];
+			}
+        }
+        return $comment_list;
+	}
+	/**
+     * 获取众筹项目的评价详情shuliang 
+     */
+	function crowd_comment_info($id, $type){
+		
+		switch($type){
+            case 1:
+            $where = " AND rank > 0";
+            break;
+            case 2:
+            $where = " AND rank IN (4,5)";
+            break;
+            case 3:
+            $where = " AND rank IN (2,3)";
+            break;
+            case 4:
+            $where = " AND rank IN (0,1)";
+            break;
+        }
+		
+		$sql = 'SELECT COUNT(*) as count FROM ' . $this->pre .
+                "crowd_comment WHERE goods_id = '$id' $where  AND status = 1 AND parent_id = 0" .
+                ' ORDER BY id DESC';
+        $result = $this->row($sql);
+        $info['sum_count'] = $result['count'];
+		
+		
+		$sql = 'SELECT COUNT(*) as count FROM ' . $this->pre .
+                "crowd_comment WHERE goods_id = '$id'   AND status = 1 AND parent_id = 0" .
+                ' ORDER BY id DESC';
+        $result = $this->row($sql);
+        $info['count'] = $result['count'];
+
+        $sql = 'SELECT COUNT(*) as count FROM ' . $this->pre .
+                "crowd_comment WHERE goods_id = '$id'  AND (rank= 5 OR rank = 4) AND status = 1 AND parent_id = 0" .
+                ' ORDER BY id DESC';
+        $result = $this->row($sql);
+        $favorable = $result['count'];
+
+        $sql = 'SELECT COUNT(*) as count FROM ' . $this->pre .
+                "crowd_comment WHERE goods_id = '$id'  AND status = 1 AND parent_id = 0 AND(rank = 2 OR rank = 3)" .
+                ' ORDER BY id DESC';
+        $result = $this->row($sql);
+        $medium = $result['count'];
+
+        $sql = 'SELECT COUNT(*) as count FROM ' . $this->pre .
+                "crowd_comment WHERE goods_id = '$id' AND status = 1 AND parent_id = 0 AND rank = 1 " .
+                ' ORDER BY id DESC';
+        $result = $this->row($sql);
+        $bad = $result['count'];
+
+        $info['favorable_count'] = $favorable; //好评数量
+        $info['medium_count'] = $medium; //中评数量
+        $info['bad_count'] = $bad; //差评数量
+        if ($info['count'] > 0) {
+            $info['favorable'] = 0;
+            if ($favorable) {
+                $info['favorable'] = round(($favorable / $info['count']) * 100);  //好评率
+            }
+            $info['medium'] = 0;
+            if ($medium) {
+                $info['medium'] = round(($medium / $info['count']) * 100); //中评
+            }
+            $info['bad'] = 0;
+            if ($bad) {
+                $info['bad'] = round(($bad / $info['count']) * 100); //差评
+            }
+        } else {
+            $info['favorable'] = 100;
+            $info['medium'] = 100;
+            $info['bad'] = 100;
+        }
+        return $info;
+		
+	}
+    
+	/**
+     * 获取众筹项目的评轮列表
+     */
+	function crowd_get_comment($goods_id,$type,$pay = 1, $num = 10, $start = 0){
+
+		
+		 if(empty($goods_id)){
+            return false;
+        }
+        switch($type){
+            case 1:
+            $where = " AND rank > 0";
+            break;
+            case 2:
+            $where = " AND rank IN (4,5)";
+            break;
+            case 3:
+            $where = " AND rank IN (2,3)";
+            break;
+            case 4:
+            $where = " AND rank IN (0,1)";
+            break;
+        }
+        if(!empty($limit)){
+            $limit = " LIMIT $limit";
+        }
+        $sql = "SELECT id,user_id,user_name,goods_id,content,add_time FROM ". $this->pre ."crowd_comment WHERE ".
+               " goods_id = '".$goods_id."' AND parent_id = 0 AND status = 1 $where ORDER BY id DESC LIMIT $start , $num";
+        $comment_list = $this->query($sql);
+        foreach($comment_list as $k => $v){
+			$comment_list[$k]['add_time'] = local_date(C('time_format'), $v['add_time']);
+			$comment_list[$k]['name'] =  $v['user_name'];
+			$comment_list[$k]['avatar'] = '';
+			$wechat_user = $this->model->table('wechat_user')->where("ect_uid=".$v['user_id'])->field('nickname,headimgurl')->find();
+			if(!empty($wechat_user)){
+				$comment_list[$k]['name'] =  $wechat_user['nickname'];
+				$comment_list[$k]['avatar'] = $wechat_user['headimgurl'];
+			}
+			$comment_list[$k]['reply'] =  $this->reply_content($v['id']);
+			
+        }
+        return $comment_list;
+		
+	}
+	
+	/**
+     * 获取众筹项目的支持人
+     */
+	function buy_num($goods_id){
+		
+		return $buy_num_list;
+	}
+	/**
+     * 获取众筹项目的评轮回复信息
+     */
+	function reply_content($parent_id){
+		$sql = "SELECT id,user_id,user_name,goods_id,content,add_time FROM ". $this->pre ."crowd_comment WHERE ".
+               " parent_id = '".$parent_id."' AND status = 1 $where ORDER BY id DESC";
+        $replyt_list = $this->query($sql);
+        foreach($replyt_list as $k => $v){
+			$replyt_list[$k]['add_time'] = local_date(C('time_format'), $v['add_time']);
+        }
+		 return $replyt_list;
+	}
+	
+	/**
+     * 获取众筹项目动态
+     */
+	function crowd_trends($goods_id){
+		$sql = "SELECT id,goods_id,content,add_time FROM ". $this->pre ."crowd_trends WHERE ".
+               " goods_id = '".$goods_id."' AND status = 1 $where ORDER BY id DESC ";
+        $trends_list = $this->query($sql);
+        foreach($trends_list as $k => $v){
+			$trends_list[$k]['add_time'] = local_date(C('time_format'), $v['add_time']);
+        }
+        return $trends_list;
+		
+	}
+	
+	/**
+     * 获取众筹项的支持者
+     */
+	function crowd_buy($goods_id){
+		$sql = "SELECT o.user_id, o.add_time, g.goods_price  FROM ". $this->pre ."order_info as o left join  ". $this->pre ."order_goods as g on o.order_id = g.order_id". " WHERE g.goods_id = '".$goods_id."' AND o.extension_code = 'crowd_buy'  ORDER BY g.rec_id DESC ";
+        $buy_list = $this->query($sql);
+        foreach($buy_list as $k => $v){
+			$buy_list[$k]['add_time'] = local_date(C('time_format'), $v['add_time']);		
+			
+			$user = $this->model->table('users')->where("user_id=".$v['user_id'])->field('user_name')->find();		
+			$wechat_user = $this->model->table('wechat_user')->where("ect_uid=".$v['user_id'])->field('nickname,headimgurl')->find();
+			if(!empty($wechat_user)){
+				$buy_list[$k]['name'] =  $wechat_user['nickname'];
+				$buy_list[$k]['avatar'] = $wechat_user['headimgurl'];
+			}else{
+				$buy_list[$k]['name'] =  $user['user_name'];
+				$buy_list[$k]['avatar'] = '';				
+			}
+        }
+
+        return $buy_list;
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
