32f4e9419a5c614ba533d022698ab45e0337e483
hanbenfa@ecmoban.com <hanbenfa@ecmoban.com>
2016-08-03 14:36:20 +0800

    众筹我的关注

--- include/apps/default/controllers/MycrowdController.class.php
+++ include/apps/default/controllers/MycrowdController.class.php
@@ -23,6 +23,8 @@ class MycrowdController extends CommonController {
     {
         parent::__construct();
 		$this->user_id = $_SESSION['user_id'];
+		// 验证登录
+		$this->check_login();
         // 用户信息
         $info = model('ClipsBase')->get_user_default($this->user_id);
         // 显示第三方API的头像
@@ -45,16 +47,57 @@ class MycrowdController extends CommonController {
 	/**
      * 关注众筹项目列表信息
      */
-    public function my_like() {
-		
-        //$recommend = model('Mycrowd')->recom_list();//获取推荐众筹
-		//$this->assign('recommend', $recommend);
+    public function crowd_like() {
+		$this->type = I('request.type') ? intval(I('request.type')) : 1 ;
+        $like = model('Mycrowd')->like_list($this->user_id, $this->type);//获取关注众筹
+		$this->assign('like', $like);
+		$this->assign('type', $this->type);
         $this->display('crowd/raise_follow.html');
     }
 	
     
 	
-	
+	/**
+     * 未登录验证
+     */
+    private function check_login() {
+        // 不需要登录的操作或自己验证是否登录（如ajax处理）的方法
+        $without = array(
+            'login',
+            'register',
+            'get_password_phone',
+            'get_password_email',
+            'get_password_question',
+            'pwd_question_name',
+            'send_pwd_email',
+            'edit_password',
+            'check_answer',
+            'logout',
+            'clear_histroy',
+            'add_collection',
+            'third_login',
+            'bind',
+            'unsetsession'
+        );
+        // 未登录处理
+        if (empty($_SESSION['user_id']) && !in_array($this->action, $without)) {
+            $url = __HOST__ . $_SERVER['REQUEST_URI'];
+            $this->redirect(url('login', array(
+                'referer' => urlencode($url)
+            )));
+            exit();
+        }
+
+        // 已经登录，不能访问的方法
+        $deny = array(
+            'login',
+            'register'
+        );
+        if (isset($_SESSION['user_id']) && $_SESSION['user_id'] > 0 && in_array($this->action, $deny)) {
+            $this->redirect(url('index/index'));
+            exit();
+        }
+    }
 	
 	
 	
--- include/apps/default/models/MycrowdModel.class.php
+++ include/apps/default/models/MycrowdModel.class.php
@@ -21,7 +21,7 @@ class MycrowdModel extends CommonModel {
      * 获取推荐众筹     
      */
 	function recom_list(){		
-		$sql = 'SELECT goods_id, cat_id, goods_name, goods_img, sum_price, total_price, start_time, time, like_num, buy_num '.'FROM '
+		$sql = 'SELECT goods_id, cat_id, goods_name, goods_img, sum_price, total_price, start_time, time '.'FROM '
 		. $this->pre . 'crowd_goods ' . "WHERE is_verify = 1 and recommend = 1 order by sort_order DESC ";
         $res = $this->query($sql);
 
@@ -56,7 +56,50 @@ class MycrowdModel extends CommonModel {
 	}
 	
 	
+	/**
+     * 获取关注众筹     
+     */
+	function like_list($user_id = 0, $type= 0){	
+
+		switch($type){
+            case 1:
+            $where = " ";                  //全部
+            break;
+            case 2:
+            $where = " AND g.status < 1";  //进行中
+            break;
+            case 3:
+            $where = " AND g.status = 1";  //已成功
+            break;
+            case 4:
+            $where = " AND g.status = 2";  //已失败
+            break;
+        }
+
 	
+		 $sql = 'SELECT g.goods_id, g.cat_id, g.goods_name, g.goods_img, g.sum_price, g.total_price, g.start_time, g.time  '.'FROM '
+		. $this->pre . 'crowd_goods as g left join ' . $this->pre  ."crowd_like as cl" . " on g.goods_id=cl.goods_id " . " WHERE g.is_verify = 1 $where and cl.user_id = '$user_id'  order by g.sort_order DESC ";
+
+        $res = $this->query($sql);
+		$goods = array();
+        foreach ($res AS $key => $row) {
+            $goods[$key]['id'] = $row['goods_id'];
+			$goods[$key]['cat_id'] = $row['cat_id'];
+            $goods[$key]['goods_name'] = $row['goods_name'];
+			$goods[$key]['time'] = $row['time'];
+            $goods[$key]['like_num'] = $row['like_num'];
+            $goods[$key]['buy_num'] = model('Crowdfunding')->crowd_buy_num($row['goods_id']);
+			$goods[$key]['start_time'] =floor((gmtime()-$row['start_time'])/86400);
+			$goods[$key]['sum_price'] = $row['sum_price'];
+            $goods[$key]['total_price'] = model('Crowdfunding')->crowd_buy_price($row['goods_id']);
+            $goods[$key]['goods_img'] = 'data/attached/crowdimage/'.$row['goods_img'];
+            $goods[$key]['url'] = url('Crowdfunding/goods_info', array('id' => $row['goods_id']));
+			$goods[$key]['bar'] = $goods[$key]['total_price']*100/$row['sum_price'];
+			$goods[$key]['bar'] = round($goods[$key]['bar'],1); //计算百分比
+			$goods[$key]['min_price'] = $this->plan_min_price($row['goods_id']); //获取方案最低价格
+        }
+        return $goods;
+	}
 	
 	
 	
