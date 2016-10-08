2d2e72b77c7851403893d2b0db6e115309792000
yangruopeng <yangruopeng@ecmoban.com>
2016-10-08 18:09:00 +0800

    优化后台商品属性排序每次编辑商品属性价格会变化

--- admin/includes/lib_goods.php
+++ admin/includes/lib_goods.php
@@ -625,7 +625,7 @@ function get_attr_list($cat_id, $goods_id = 0)
             "LEFT JOIN " .$GLOBALS['ecs']->table('goods_attr'). " AS v ".
             "ON v.attr_id = a.attr_id AND v.goods_id = '$goods_id' ".
             "WHERE a.cat_id = " . intval($cat_id) ." OR a.cat_id = 0 ".
-            "ORDER BY a.sort_order, a.attr_type, a.attr_id, v.attr_price, v.goods_attr_id";
+            "ORDER BY a.sort_order, a.attr_type, v.goods_attr_id, a.attr_id ";
 
     $row = $GLOBALS['db']->GetAll($sql);
 
@@ -997,7 +997,8 @@ function product_goods_attr_list($goods_id)
         return array();  //$goods_id不能为空
     }
 
-    $sql = "SELECT goods_attr_id, attr_value FROM " . $GLOBALS['ecs']->table('goods_attr') . " WHERE goods_id = '$goods_id'";
+    $sql = "SELECT goods_attr_id, attr_value FROM " . $GLOBALS['ecs']->table('goods_attr') . " WHERE goods_id = '$goods_id' order by goods_attr_id DESC";
+
     $results = $GLOBALS['db']->getAll($sql);
 
     $return_arr = array();
@@ -1029,7 +1030,8 @@ function get_goods_specifications_list($goods_id)
                     ON a.attr_id = g.attr_id
             WHERE goods_id = '$goods_id'
             AND a.attr_type = 1
-            ORDER BY g.attr_id ASC";
+            ORDER BY g.goods_attr_id ASC";
+
     $results = $GLOBALS['db']->getAll($sql);
 
     return $results;
