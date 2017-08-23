6ccd446e9e1bb14d8d7d40940b48c9e4f29e12ba
PC-20170517MHUW\Administrator <xiaorui@ecmoban.com>
2017-08-23 16:15:25 +0800

    提交安装sql修改

--- install/sqldata/mobile.sql
+++ install/sqldata/mobile.sql
@@ -301,6 +301,59 @@ CREATE TABLE IF NOT EXISTS `ecs_drp_shop` (
   PRIMARY KEY (`id`,`user_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
 
+
+--
+-- 表的结构 `ecs_cart_combo`
+--
+
+CREATE TABLE IF NOT EXISTS `ecs_cart_combo` (
+  `rec_id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `session_id` char(32) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT '',
+  `goods_id` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `goods_sn` varchar(60) NOT NULL DEFAULT '',
+  `product_id` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `goods_name` varchar(120) NOT NULL DEFAULT '',
+  `market_price` decimal(10,2) unsigned NOT NULL DEFAULT '0.00',
+  `goods_price` decimal(10,2) NOT NULL DEFAULT '0.00',
+  `goods_number` smallint(5) unsigned NOT NULL DEFAULT '0',
+  `goods_attr` text NOT NULL,
+  `is_real` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `extension_code` varchar(30) NOT NULL DEFAULT '',
+  `parent_id` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `rec_type` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `is_gift` smallint(5) unsigned NOT NULL DEFAULT '0',
+  `is_shipping` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `can_handsel` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `goods_attr_id` varchar(255) NOT NULL DEFAULT '',
+  `group_id` varchar(255) NOT NULL ,
+  PRIMARY KEY (`rec_id`),
+  KEY `session_id` (`session_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+
+-- --------------------------------------------------------
+--
+-- 表的结构 `ecs_term_relationship`
+--
+
+CREATE TABLE IF NOT EXISTS `ecs_term_relationship` (
+  `relation_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `object_type` char(30) NOT NULL DEFAULT '',
+  `object_group` char(30) NOT NULL DEFAULT '',
+  `object_id` int(11) NOT NULL,
+  `item_key1` varchar(60) DEFAULT NULL,
+  `item_value1` varchar(60) DEFAULT NULL,
+  `item_key2` varchar(60) DEFAULT NULL,
+  `item_value2` varchar(60) DEFAULT NULL,
+  `item_key3` varchar(60) DEFAULT NULL,
+  `item_value3` varchar(60) DEFAULT NULL,
+  `item_key4` varchar(60) DEFAULT NULL,
+  `item_value4` varchar(60) DEFAULT NULL,
+  PRIMARY KEY (`relation_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=100 ;
+
+
 -- ----------------------------
 -- Table structure for `ecs_drp_bank`
 -- ----------------------------
@@ -375,6 +428,21 @@ CREATE TABLE IF NOT EXISTS `ecs_drp_apply` (
 -- Records of ecs_drp_config
 -- ----------------------------
 
+CREATE TABLE IF NOT EXISTS `ecs_drp_invalid_log` (
+  `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `user_money` decimal(10,2) NOT NULL DEFAULT '0.00',
+  `pay_points` mediumint(9) NOT NULL DEFAULT '0',
+  `change_time` int(10) unsigned NOT NULL DEFAULT '0',
+  `change_desc` varchar(255) NOT NULL DEFAULT '',
+  `change_type` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `invalid_desc`  text COMMENT '无效说明',
+  `order_id` int(10) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+
 INSERT INTO `ecs_drp_config` VALUES ('1', '', 'apply', '温馨提示', '申请分销商时，提示用户需要注意的信息', 'textarea', '亲，您的佣金由三部分组成：<br>1.本店所销售的商品，我所获得的佣金（即本店销售佣金）<br>2.下级分店所销售的商品，我所获得的佣金（即一级分店佣金）<br>3.下级分店发展的分店所销售的商品，我所获得的佣金（即二级分店佣金）');
 INSERT INTO `ecs_drp_config` VALUES ('2', '', 'novice', '新手必读', '分销商申请成功后，用户要注意的事项', 'textarea', '1、开微店收入来源之一：您已成功注册微店，已经取得整个商城的商品销售权，只要有人在您的微店购物，即可获得“本店销售佣金”。<br>2、开微店收入来源之二：邀请您的朋友注册微店，他就会成为你的分销商，他店内销售的商品，您即可获得“一级分店佣金”。<br>3、开微店收入来源之三：您的分销商邀请他的朋友注册微店，他店内销售的商品，您即可获得“二级分店佣金”。');
 INSERT INTO `ecs_drp_config` VALUES ('3', '', 'fxts', '间隔', '下单并付款之后经过间隔天数才可以对订单分成', 'text', '1');
@@ -390,7 +458,164 @@ INSERT INTO `ecs_drp_config` VALUES ('12', '', 'custom_distribution', '自定义
 ALTER TABLE `ecs_users` ADD COLUMN `apply_sale` int(1) unsigned NOT NULL DEFAULT '0';
 
 -- /*DRP_END*/
+--
+-- 表的结构 `ecs_order_return`
+--
+DROP TABLE IF EXISTS `ecs_order_return`;
+
+CREATE TABLE IF NOT EXISTS `ecs_order_return` (
+  `ret_id` int(10) NOT NULL AUTO_INCREMENT COMMENT '退换货id',
+  `service_sn` varchar(20) NOT NULL COMMENT '服务订单编号',
+  `goods_id` int(13) NOT NULL COMMENT '商品唯一id',
+  `user_id` int(10) NOT NULL COMMENT '用户id',
+  `rec_id` int(10) NOT NULL COMMENT '订单商品唯一id',
+  `order_id` mediumint(8) NOT NULL COMMENT '所属订单号',
+  `order_sn` varchar(20) NOT NULL,
+  `service_id` int(2) NOT NULL,
+  `cause_id` int(10) NOT NULL COMMENT '退换货原因',
+  `add_time` varchar(120) NOT NULL COMMENT '插入时间',
+  `should_return` decimal(10,2) NOT NULL COMMENT '应退金额',
+  `actual_return` decimal(10,2) NOT NULL COMMENT '实退金额',
+  `remark` text NOT NULL COMMENT '备注',
+  `country` smallint(5) NOT NULL COMMENT '国家',
+  `province` smallint(5) NOT NULL COMMENT '省份',
+  `city` smallint(5) NOT NULL COMMENT '城市',
+  `district` smallint(5) NOT NULL COMMENT '区',
+  `addressee` varchar(30) NOT NULL COMMENT '收件人',
+  `phone` varchar(20) NOT NULL COMMENT '联系电话',
+  `address` varchar(100) NOT NULL COMMENT '详细地址',
+  `zipcode` int(6) DEFAULT NULL COMMENT '邮编',
+  `return_status` tinyint(3) NOT NULL COMMENT '退换货状态',
+  `refund_status` tinyint(3) NOT NULL COMMENT '退款状态',
+  `back_shipping_name` varchar(30) NOT NULL COMMENT '退回快递名称',
+  `back_other_shipping` varchar(30) NOT NULL COMMENT '其他快递名称',
+  `back_invoice_no` varchar(50) NOT NULL COMMENT '退回快递单号',
+  `out_shipping_name` varchar(30) NOT NULL COMMENT '换出快递名称',
+  `out_invoice_no` varchar(50) NOT NULL COMMENT '换出快递单号',
+  `seller_id` int(11) NOT NULL,
+  `is_check` tinyint(1) NOT NULL COMMENT '是否审核',
+  `to_buyer` varchar(255) NOT NULL,
+  PRIMARY KEY (`ret_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COMMENT='商品退货表' AUTO_INCREMENT=4 ;
+
+
+--
+-- 表的结构 `ecs_return_action`
+--
+DROP TABLE IF EXISTS `ecs_return_action`;
+
+CREATE TABLE IF NOT EXISTS `ecs_return_action` (
+  `action_id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `ret_id` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `action_user` varchar(30) NOT NULL DEFAULT '',
+  `return_status` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `refund_status` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `is_check` tinyint(2) NOT NULL COMMENT '审核是否通过',
+  `action_place` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `action_note` varchar(255) NOT NULL DEFAULT '',
+  `action_info` varchar(255) NOT NULL COMMENT '操作介绍',
+  `log_time` int(11) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`action_id`),
+  KEY `order_id` (`ret_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=8 ;
+
+
+--
+-- 表的结构 `ecs_return_cause`
+--
+DROP TABLE IF EXISTS `ecs_return_cause`;
+
+CREATE TABLE IF NOT EXISTS `ecs_return_cause` (
+  `cause_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '自增id',
+  `cause_name` varchar(50) NOT NULL COMMENT '退换货原因',
+  `parent_id` int(11) NOT NULL COMMENT '父级id',
+  `sort_order` int(10) NOT NULL COMMENT '排序',
+  `is_show` tinyint(3) NOT NULL COMMENT '是否显示',
+  PRIMARY KEY (`cause_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COMMENT='退换货原因说明' AUTO_INCREMENT=24 ;
+
+--
+-- 转存表中的数据 `ecs_return_cause`
+--
+INSERT INTO `ecs_return_cause` (`cause_name`, `parent_id`, `sort_order`, `is_show`) VALUES
+('颜色问题', 0, 50, 1),
+('质量问题', 0, 50, 1);
+
+--
+-- 表的结构 `ecs_return_goods`
+--
+DROP TABLE IF EXISTS `ecs_return_goods`;
+
+CREATE TABLE IF NOT EXISTS `ecs_return_goods` (
+  `rg_id` int(10) NOT NULL AUTO_INCREMENT,
+  `rec_id` mediumint(8) unsigned NOT NULL,
+  `goods_id` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `product_id` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `product_sn` varchar(60) DEFAULT NULL,
+  `goods_name` varchar(120) DEFAULT NULL,
+  `brand_name` varchar(60) DEFAULT NULL,
+  `goods_sn` varchar(60) DEFAULT NULL,
+  `is_real` tinyint(1) unsigned DEFAULT '0',
+  `goods_attr` text,
+  `goods_price` decimal(10,2) NOT NULL DEFAULT '0.00',
+  `return_type` tinyint(3) NOT NULL,
+  `back_num` smallint(6) NOT NULL,
+  `out_num` smallint(6) NOT NULL,
+  `out_attr` varchar(100) NOT NULL,
+  `refund` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`rg_id`),
+  KEY `goods_id` (`goods_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+
+
+--
+-- 表的结构 `ecs_aftermarket_attachments`
+--
+DROP TABLE IF EXISTS `ecs_aftermarket_attachments`;
+
+CREATE TABLE IF NOT EXISTS `ecs_aftermarket_attachments` (
+  `img_id` mediumint(8) NOT NULL AUTO_INCREMENT,
+  `rec_id` mediumint(8) NOT NULL,
+  `img_url` varchar(255) NOT NULL,
+  `goods_id` mediumint(8) NOT NULL,
+  UNIQUE KEY `img_id` (`img_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=17 ;
+
+--
+-- 表的结构 `ecs_service_type`
+--
+DROP TABLE IF EXISTS `ecs_service_type`;
+
+CREATE TABLE IF NOT EXISTS `ecs_service_type` (
+  `service_id` int(10) NOT NULL AUTO_INCREMENT,
+  `service_name` varchar(60) NOT NULL,
+  `service_desc` text NOT NULL,
+  `received_days` mediumint(4) NOT NULL,
+  `unreceived_days` mediumint(6) NOT NULL,
+  `is_show` tinyint(1) NOT NULL,
+  `sort_order` tinyint(3) NOT NULL,
+  `service_type` tinyint(1) NOT NULL COMMENT '服务类型',
+  PRIMARY KEY (`service_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+
+--
+-- 转存表中的数据 `ecs_service_type`
+--
+
+INSERT INTO `ecs_service_type` (`service_id`, `service_name`, `service_desc`, `received_days`, `unreceived_days`, `is_show`, `sort_order`, `service_type`) VALUES
+(1, '退货退款', '已收到货，需要退还已收到的货物1', 7, 8, 1, 9, 1),
+(3, '换货', '对已收到的货物不满意，联系卖家协商换货', 7, 10, 1, 3, 3);
+
 
 ALTER TABLE `ecs_brand` ADD COLUMN `brand_banner` varchar(80)  DEFAULT '';
 ALTER TABLE `ecs_goods_activity` ADD COLUMN `touch_img` VARCHAR (50)  DEFAULT '';
 ALTER TABLE `ecs_favourable_activity` ADD COLUMN `touch_img` VARCHAR (50)  DEFAULT '';
+
+ALTER TABLE `ecs_cart`
+  ADD COLUMN `group_id` varchar(255) NOT NULL ;
+ALTER TABLE `ecs_group_goods`
+  ADD COLUMN `group_id` tinyint(3) unsigned NOT NULL DEFAULT '0',
+ALTER TABLE `ecs_goods`
+  ADD COLUMN `virtual_sales` varchar( 10 ) NOT NULL DEFAULT '0'
+ALTER TABLE  `ecs_order_info` 
+  ADD COLUMN `inv_text_id` varchar(120) NOT NULL DEFAULT '' AFTER `inv_content`;
--- install/sqldata/structure.sql
+++ install/sqldata/structure.sql
@@ -480,40 +480,10 @@ CREATE TABLE IF NOT EXISTS `ecs_cart` (
   `is_shipping` tinyint(1) unsigned NOT NULL DEFAULT '0',
   `can_handsel` tinyint(3) unsigned NOT NULL DEFAULT '0',
   `goods_attr_id` varchar(255) NOT NULL DEFAULT '',
-  `group_id` varchar(255) NOT NULL ,
   PRIMARY KEY (`rec_id`),
   KEY `session_id` (`session_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
--- --------------------------------------------------------
---
--- 表的结构 `ecs_cart_combo`
---
-
-CREATE TABLE IF NOT EXISTS `ecs_cart_combo` (
-  `rec_id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
-  `user_id` mediumint(8) unsigned NOT NULL DEFAULT '0',
-  `session_id` char(32) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT '',
-  `goods_id` mediumint(8) unsigned NOT NULL DEFAULT '0',
-  `goods_sn` varchar(60) NOT NULL DEFAULT '',
-  `product_id` mediumint(8) unsigned NOT NULL DEFAULT '0',
-  `goods_name` varchar(120) NOT NULL DEFAULT '',
-  `market_price` decimal(10,2) unsigned NOT NULL DEFAULT '0.00',
-  `goods_price` decimal(10,2) NOT NULL DEFAULT '0.00',
-  `goods_number` smallint(5) unsigned NOT NULL DEFAULT '0',
-  `goods_attr` text NOT NULL,
-  `is_real` tinyint(1) unsigned NOT NULL DEFAULT '0',
-  `extension_code` varchar(30) NOT NULL DEFAULT '',
-  `parent_id` mediumint(8) unsigned NOT NULL DEFAULT '0',
-  `rec_type` tinyint(1) unsigned NOT NULL DEFAULT '0',
-  `is_gift` smallint(5) unsigned NOT NULL DEFAULT '0',
-  `is_shipping` tinyint(1) unsigned NOT NULL DEFAULT '0',
-  `can_handsel` tinyint(3) unsigned NOT NULL DEFAULT '0',
-  `goods_attr_id` varchar(255) NOT NULL DEFAULT '',
-  `group_id` varchar(255) NOT NULL ,
-  PRIMARY KEY (`rec_id`),
-  KEY `session_id` (`session_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 --
@@ -592,26 +562,6 @@ CREATE TABLE IF NOT EXISTS `ecs_comment` (
   KEY `id_value` (`id_value`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
--- --------------------------------------------------------
---
--- 表的结构 `ecs_term_relationship`
---
-
-CREATE TABLE IF NOT EXISTS `ecs_term_relationship` (
-  `relation_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `object_type` char(30) NOT NULL DEFAULT '',
-  `object_group` char(30) NOT NULL DEFAULT '',
-  `object_id` int(11) NOT NULL,
-  `item_key1` varchar(60) DEFAULT NULL,
-  `item_value1` varchar(60) DEFAULT NULL,
-  `item_key2` varchar(60) DEFAULT NULL,
-  `item_value2` varchar(60) DEFAULT NULL,
-  `item_key3` varchar(60) DEFAULT NULL,
-  `item_value3` varchar(60) DEFAULT NULL,
-  `item_key4` varchar(60) DEFAULT NULL,
-  `item_value4` varchar(60) DEFAULT NULL,
-  PRIMARY KEY (`relation_id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=100 ;
 
 -- --------------------------------------------------------
 --
@@ -850,7 +800,6 @@ CREATE TABLE IF NOT EXISTS `ecs_goods` (
   `goods_number` smallint(5) unsigned NOT NULL DEFAULT '0',
   `goods_weight` decimal(10,3) unsigned NOT NULL DEFAULT '0.000',
   `market_price` decimal(10,2) unsigned NOT NULL DEFAULT '0.00',
-  `virtual_sales` varchar( 10 ) NOT NULL DEFAULT '0',
   `shop_price` decimal(10,2) unsigned NOT NULL DEFAULT '0.00',
   `promote_price` decimal(10,2) unsigned NOT NULL DEFAULT '0.00',
   `promote_start_date` int(11) unsigned NOT NULL DEFAULT '0',
@@ -1001,7 +950,6 @@ CREATE TABLE IF NOT EXISTS `ecs_group_goods` (
   `goods_id` mediumint(8) unsigned NOT NULL DEFAULT '0',
   `goods_price` decimal(10,2) unsigned NOT NULL DEFAULT '0.00',
   `admin_id` tinyint(3) unsigned NOT NULL DEFAULT '0',
-  `group_id` tinyint(3) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`parent_id`,`goods_id`,`admin_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
