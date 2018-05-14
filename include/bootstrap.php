b4f2fef61b618fe6c99eefac16cb7347ae8ff918
xiaorui <xiaorui@ecmoban.com>
2018-05-14 16:14:43 +0800

    提交版本

--- include/bootstrap.php
+++ include/bootstrap.php
@@ -17,8 +17,8 @@
 defined('IN_ECTOUCH') or die('Deny Access');
 header("Content-Type:text/html;charset=utf-8");
 define('APPNAME', 'ECTouch');
-define('VERSION', '2.27.31');
-define('RELEASE', '20180131');
+define('VERSION', '2.31.14');
+define('RELEASE', '20180514');
 defined('BASE_PATH') or define('BASE_PATH', dirname(__FILE__) . '/');
 defined('ROOT_PATH') or define('ROOT_PATH', str_replace('\\', '/', realpath(dirname(__FILE__) . '/../')) . '/');
 defined('APP_PATH') or define('APP_PATH', BASE_PATH . 'apps/');
